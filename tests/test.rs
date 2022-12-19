ada46a7f60132049dfa5eda8adf148cb74395571
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-12-19 18:53:18 +0200

    Test module

---
+++ tests/test.rs
@@ -0,0 +1,21 @@
+use appcui::graphics::*;
+use appcui::terminal::*;
+
+#[test]
+fn test_1() {
+    let term = TerminalType::new(TerminalType::WindowsConsole);
+    assert!(term.is_some());
+    let mut term = term.unwrap();
+    let mut s = Surface::new(term.get_width(), term.get_height());
+    
+    s.clear(Character::new(SpecialChar::CircleEmpty, Color::White, Color::DarkBlue,Attribute::None));
+    s.fill_rect(5, 5, 7, 7, Character::with_char(' '));
+    s.fill_rect(15, 5, 20, 7, Character::new('x',Color::Red, Color::Black, Attribute::Underline));
+    s.set_origin(30, 5);
+    s.fill_rect_with_size(35, 5, 40, 7, Character::new(SpecialChar::Block25,Color::DarkRed, Color::Black, Attribute::None));
+    s.reset_origin();
+    s.fill_horizontal_line(1, 3, 5, Character::new('|', Color::Yellow, Color::Red, Attribute::None));
+
+    term.update_screen(&s);
+
+}
