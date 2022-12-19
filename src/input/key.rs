25eb36bc3a83e60ed6e6d94fc120779dae9fb555
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-12-19 19:31:30 +0200

    Input module (keys)

--- src/input/key.rs
+++ src/input/key.rs
@@ -1,5 +1,19 @@
-struct Key {
-    pub code: KeyCode;
-    pub modifier: KeyModifier;
-    pub character: char;
+use super::KeyCode;
+use super::KeyModifier;
+
+#[derive(Copy,Clone)]
+pub struct Key {
+    pub code: KeyCode,
+    pub modifier: KeyModifier,
+    pub character: char,
+}
+
+impl Key {
+    pub fn new(code: KeyCode, modifier:KeyModifier, character: char) -> Key {
+        Key {
+            code: code,
+            modifier: modifier,
+            character: character
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod graphics;
 pub mod terminal;
+pub mod input;
