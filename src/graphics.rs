f27c250f93fe0fb5615e95fa8747874798d5312e
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-12-19 18:53:03 +0200

    graphics module

---
+++ src/graphics.rs
@@ -0,0 +1,15 @@
+mod character;
+mod color;
+mod attribute;
+mod surface;
+mod line_type;
+
+
+pub use self::character::Character;
+pub use self::character::SpecialChar;
+pub use self::color::Color;
+pub use self::attribute::Attribute;
+pub use self::line_type::LineType;
+pub use self::surface::Surface;
+
+
