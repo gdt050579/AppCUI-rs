67a62b3911054def31e8808649d016c148ad9b6d
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-01-05 07:15:34 +0200

    Change layout coordonate and size to fit a 16 bit value

--- appcui/src/controls/layout/coordonate.rs
+++ appcui/src/controls/layout/coordonate.rs
@@ -1,22 +1,31 @@
 use crate::utils::{KeyValuePair, ValueType};
 
-pub (super) enum Coordonate {
-    Absolute(i32),
-    Percentage(i32)
+pub(super) enum Coordonate {
+    Absolute(i16),
+    Percentage(i16),
 }
 impl Coordonate {
-    pub (super) fn to_absolute_coordonate(&self, parent_size: u32)->i32 {
-        match self
-        {
-            Coordonate::Absolute(v) => { return *v; },
-            Coordonate::Percentage(v) => { return v * (parent_size as i32) / 10000; }
+    pub(super) fn to_absolute_coordonate(&self, parent_size: u32) -> i32 {
+        match self {
+            Coordonate::Absolute(v) => {
+                return (*v) as i32;
+            }
+            Coordonate::Percentage(v) => {
+                return ((*v) as i32) * (parent_size as i32) / 10000i32;
+            }
         }
     }
-    pub (super) fn new(value: &KeyValuePair) -> Self {
+    pub(super) fn new(value: &KeyValuePair) -> Self {
         match value.value_type {
-            ValueType::Number => { return Coordonate::Absolute(value.numerical_value); }
-            ValueType::Percentage => { return Coordonate::Percentage(value.numerical_value); }
-            _ => { return Coordonate::Absolute(0); }
+            ValueType::Number => {
+                return Coordonate::Absolute(value.numerical_value.clamp(-30000, 30000) as i16);
+            }
+            ValueType::Percentage => {
+                return Coordonate::Percentage(value.numerical_value.clamp(-30000, 30000) as i16);
+            }
+            _ => {
+                return Coordonate::Absolute(0);
+            }
         }
     }
 }
--- appcui/src/controls/layout/size.rs
+++ appcui/src/controls/layout/size.rs
@@ -1,22 +1,31 @@
 use crate::utils::{KeyValuePair, ValueType};
 
-pub (super) enum Size {
-    Absolute(u32),
-    Percentage(u32)
+pub(super) enum Size {
+    Absolute(u16),
+    Percentage(u16),
 }
 impl Size {
-    pub (super) fn to_absolute_coordonate(&self, parent_size: u32)->u32 {
-        match self
-        {
-            Size::Absolute(v) => { return *v; },
-            Size::Percentage(v) => { return v * parent_size  / 10000u32; }
+    pub(super) fn to_absolute_coordonate(&self, parent_size: u32) -> u32 {
+        match self {
+            Size::Absolute(v) => {
+                return (*v) as u32;
+            }
+            Size::Percentage(v) => {
+                return ((*v) as u32) * parent_size / 10000u32;
+            }
         }
     }
-    pub (super) fn new(value: &KeyValuePair) -> Self {
+    pub(super) fn new(value: &KeyValuePair) -> Self {
         match value.value_type {
-            ValueType::Number => { return Size::Absolute(value.numerical_value as u32); }
-            ValueType::Percentage => { return Size::Percentage(value.numerical_value as u32); }
-            _ => { return Size::Absolute(0); }
+            ValueType::Number => {
+                return Size::Absolute(value.numerical_value.clamp(0, 30000) as u16);
+            }
+            ValueType::Percentage => {
+                return Size::Percentage(value.numerical_value.clamp(0, 30000) as u16);
+            }
+            _ => {
+                return Size::Absolute(0);
+            }
         }
-    }    
+    }
 }
