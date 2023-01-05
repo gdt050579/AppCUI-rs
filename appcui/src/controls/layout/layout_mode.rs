0b1ffa3a17a057cddb782811a711de5ce56127e9
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-01-05 17:09:21 +0200

    XY + Width/Height layout

--- appcui/src/controls/layout/layout_mode.rs
+++ appcui/src/controls/layout/layout_mode.rs
@@ -1,18 +1,17 @@
 use super::Alignament;
+use super::Coordonate;
 use super::LayoutParameters;
 use super::LayoutUsedParams;
-use super::Coordonate;
 use super::Size;
 
-#[derive(Copy,Clone)]
-pub (super) struct PointAndSizeLayout
-{
+#[derive(Copy, Clone)]
+pub(super) struct PointAndSizeLayout {
     pub x: Coordonate,
     pub y: Coordonate,
     pub width: Size,
     pub height: Size,
     pub align: Alignament,
-    pub anchor: Alignament
+    pub anchor: Alignament,
 }
 
 pub(super) enum LayoutMode {
@@ -30,27 +29,74 @@ pub(super) enum LayoutMode {
 }
 impl LayoutMode {
     fn new_docked_layout(params: &LayoutParameters) -> LayoutMode {
-        if params.used_params.contains_one(LayoutUsedParams::LEFT|LayoutUsedParams::RIGHT|LayoutUsedParams::TOP|LayoutUsedParams::BOTTOM|LayoutUsedParams::LEFT|LayoutUsedParams::RIGHT) {
+        if params.used_params.contains_one(
+            LayoutUsedParams::X
+                | LayoutUsedParams::Y
+                | LayoutUsedParams::TOP
+                | LayoutUsedParams::BOTTOM
+                | LayoutUsedParams::LEFT
+                | LayoutUsedParams::RIGHT,
+        ) {
             panic!("When dock|d parameter is used, none of the position (x,y) or anchor (left,right,bottom,top) parameters can not be uesd");
         }
         if params.used_params.contains_one(LayoutUsedParams::ALIGN) {
             panic!("When dock|d parameter is used, 'align' parameter can not be used !");
-        }     
+        }
         // if width or height are not present, default them to 100%
-        
-        LayoutMode::PointAndSize(
-            PointAndSizeLayout {
-                x: Coordonate::Absolute(0),
-                y: Coordonate::Absolute(0),
-                width: if params.used_params.contains(LayoutUsedParams::WIDTH) { params.width } else { Size::Percentage(10000) },
-                height: if params.used_params.contains(LayoutUsedParams::HEIGHT) { params.height } else { Size::Percentage(10000) },
-                align: params.dock,
-                anchor: params.dock
-            }
-        )
+
+        LayoutMode::PointAndSize(PointAndSizeLayout {
+            x: Coordonate::Absolute(0),
+            y: Coordonate::Absolute(0),
+            width: if params.used_params.contains(LayoutUsedParams::WIDTH) {
+                params.width
+            } else {
+                Size::Percentage(10000)
+            },
+            height: if params.used_params.contains(LayoutUsedParams::HEIGHT) {
+                params.height
+            } else {
+                Size::Percentage(10000)
+            },
+            align: params.dock,
+            anchor: params.dock,
+        })
     }
     fn new_XYWH_layout(params: &LayoutParameters) -> LayoutMode {
-        todo!();
+        // it is assume that DOCK|D is not set (as it was process early in ProcessDockedLayout)
+        // if X and Y are set --> Left, Right, Top and Bottom should not be set
+        if params.used_params.contains_one(
+            LayoutUsedParams::LEFT
+                | LayoutUsedParams::RIGHT
+                | LayoutUsedParams::TOP
+                | LayoutUsedParams::BOTTOM,
+        ) {
+            panic!("When (x,y) parameters are used, none of the anchor (left,right,bottom,top) parameters can not be used");
+        }
+
+        // if width or height are not present, they are defaulted to 1 character
+        // if align is not presented, it is defaulted to TopLeft
+        // anchor is always TopLeft
+
+        LayoutMode::PointAndSize(PointAndSizeLayout {
+            x: params.x,
+            y: params.y,
+            width: if params.used_params.contains(LayoutUsedParams::WIDTH) {
+                params.width
+            } else {
+                Size::Absolute(1)
+            },
+            height: if params.used_params.contains(LayoutUsedParams::HEIGHT) {
+                params.height
+            } else {
+                Size::Absolute(1)
+            },
+            align: if params.used_params.contains(LayoutUsedParams::ALIGN) {
+                params.align
+            } else {
+                Alignament::TopLeft
+            },
+            anchor: Alignament::TopLeft,
+        })
     }
     fn new_corner_anchor_layout(params: &LayoutParameters, align: Alignament) -> LayoutMode {
         todo!();
@@ -96,55 +142,73 @@ impl LayoutMode {
             & (LayoutUsedParams::LEFT
                 | LayoutUsedParams::TOP
                 | LayoutUsedParams::RIGHT
-                | LayoutUsedParams::BOTTOM)).get_value();
-        const LEFT_TOP_ANCHOR: u16 = LayoutUsedParams::LEFT.get_value() | LayoutUsedParams::TOP.get_value();
-        const RIGHT_TOP_ANCHOR: u16 = LayoutUsedParams::RIGHT.get_value() | LayoutUsedParams::TOP.get_value();
-        const LEFT_BOTTOM_ANCHOR: u16 = LayoutUsedParams::LEFT.get_value() | LayoutUsedParams::BOTTOM.get_value();
-        const RIGHT_BOTTOM_ANCHOR: u16 = LayoutUsedParams::RIGHT.get_value() | LayoutUsedParams::BOTTOM.get_value();
-        const HORIZONTAL_ANCHOR: u16 =LayoutUsedParams::LEFT.get_value() | LayoutUsedParams::RIGHT.get_value();
-        const VERTICAL_ANCHOR: u16 = LayoutUsedParams::TOP.get_value() | LayoutUsedParams::BOTTOM.get_value();
-        const LTR_ANCHOR: u16 = LayoutUsedParams::LEFT.get_value() | LayoutUsedParams::TOP.get_value() | LayoutUsedParams::RIGHT.get_value();
-        const LBR_ANCHOR: u16 = LayoutUsedParams::LEFT.get_value() | LayoutUsedParams::BOTTOM.get_value() | LayoutUsedParams::RIGHT.get_value();
-        const TLB_ANCHOR: u16 = LayoutUsedParams::TOP.get_value() | LayoutUsedParams::LEFT.get_value() | LayoutUsedParams::BOTTOM.get_value();
-        const TRB_ANCHOR: u16 = LayoutUsedParams::TOP.get_value() | LayoutUsedParams::RIGHT.get_value() | LayoutUsedParams::BOTTOM.get_value();
-        const LTRB_ANCHOR: u16 = LayoutUsedParams::LEFT.get_value() | LayoutUsedParams::TOP.get_value() | LayoutUsedParams::RIGHT.get_value() | LayoutUsedParams::BOTTOM.get_value();
+                | LayoutUsedParams::BOTTOM))
+            .get_value();
+        const LEFT_TOP_ANCHOR: u16 =
+            LayoutUsedParams::LEFT.get_value() | LayoutUsedParams::TOP.get_value();
+        const RIGHT_TOP_ANCHOR: u16 =
+            LayoutUsedParams::RIGHT.get_value() | LayoutUsedParams::TOP.get_value();
+        const LEFT_BOTTOM_ANCHOR: u16 =
+            LayoutUsedParams::LEFT.get_value() | LayoutUsedParams::BOTTOM.get_value();
+        const RIGHT_BOTTOM_ANCHOR: u16 =
+            LayoutUsedParams::RIGHT.get_value() | LayoutUsedParams::BOTTOM.get_value();
+        const HORIZONTAL_ANCHOR: u16 =
+            LayoutUsedParams::LEFT.get_value() | LayoutUsedParams::RIGHT.get_value();
+        const VERTICAL_ANCHOR: u16 =
+            LayoutUsedParams::TOP.get_value() | LayoutUsedParams::BOTTOM.get_value();
+        const LTR_ANCHOR: u16 = LayoutUsedParams::LEFT.get_value()
+            | LayoutUsedParams::TOP.get_value()
+            | LayoutUsedParams::RIGHT.get_value();
+        const LBR_ANCHOR: u16 = LayoutUsedParams::LEFT.get_value()
+            | LayoutUsedParams::BOTTOM.get_value()
+            | LayoutUsedParams::RIGHT.get_value();
+        const TLB_ANCHOR: u16 = LayoutUsedParams::TOP.get_value()
+            | LayoutUsedParams::LEFT.get_value()
+            | LayoutUsedParams::BOTTOM.get_value();
+        const TRB_ANCHOR: u16 = LayoutUsedParams::TOP.get_value()
+            | LayoutUsedParams::RIGHT.get_value()
+            | LayoutUsedParams::BOTTOM.get_value();
+        const LTRB_ANCHOR: u16 = LayoutUsedParams::LEFT.get_value()
+            | LayoutUsedParams::TOP.get_value()
+            | LayoutUsedParams::RIGHT.get_value()
+            | LayoutUsedParams::BOTTOM.get_value();
         match anchors {
-            LEFT_TOP_ANCHOR  => {
+            LEFT_TOP_ANCHOR => {
                 return LayoutMode::new_corner_anchor_layout(&params_list, Alignament::TopLeft);
-            },
+            }
             RIGHT_TOP_ANCHOR => {
                 return LayoutMode::new_corner_anchor_layout(&params_list, Alignament::TopRight);
-            },
+            }
             RIGHT_BOTTOM_ANCHOR => {
                 return LayoutMode::new_corner_anchor_layout(&params_list, Alignament::BottomRight);
-            },
+            }
             LEFT_BOTTOM_ANCHOR => {
                 return LayoutMode::new_corner_anchor_layout(&params_list, Alignament::BottomLeft);
-            },
+            }
             HORIZONTAL_ANCHOR => {
                 return LayoutMode::new_horizontal_anchor_layout(&params_list);
-            },
+            }
             VERTICAL_ANCHOR => {
                 return LayoutMode::new_vertical_anchor_layout(&params_list);
-            },
+            }
             LTR_ANCHOR => {
                 return LayoutMode::new_LTR_anchors_layout(&params_list);
-            },
-            LBR_ANCHOR=> {
+            }
+            LBR_ANCHOR => {
                 return LayoutMode::new_LBR_anchors_layout(&params_list);
-            },
+            }
             TLB_ANCHOR => {
                 return LayoutMode::new_TLB_anchors_layout(&params_list);
-            },
-            TRB_ANCHOR=> {
+            }
+            TRB_ANCHOR => {
                 return LayoutMode::new_TRB_anchors_layout(&params_list);
-            },   
+            }
             LTRB_ANCHOR => {
                 return LayoutMode::new_LTRB_anchors_layout(&params_list);
-            },  
+            }
             _ => {
                 panic!("Invalid format: {} --> this combination can not be used to create a layout for a control ", format);
-            }       
+            }
         }
     }
 }
