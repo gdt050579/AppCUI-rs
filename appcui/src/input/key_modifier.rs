01e882aac864d82ae36f8acddd3c9a7691361b46
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-01-08 09:01:28 +0200

    get_name methods for key and key modifier

--- appcui/src/input/key_modifier.rs
+++ appcui/src/input/key_modifier.rs
@@ -6,3 +6,23 @@ pub enum KeyModifier {
     Ctrl = 0x02,
     Shift = 0x04
 }
+
+static KEY_NAME: [&str;8] = [
+    /* 0 */ "",
+    /* 1 */ "Alt+",
+    /* 2 */ "Ctrl+",
+    /* 3 */ "Ctrl+Alt+",
+    /* 4 */ "Shift+",
+    /* 5 */ "Alt+Shift+",
+    /* 6 */ "Ctrl+Shift+",
+    /* 7 */ "Ctrl+Alt+Shift+",
+];
+
+impl KeyModifier {
+    pub fn get_name(&self)->&'static str {
+        if self.value<8 {
+            return KEY_NAME[self.value as usize];
+        }
+        return "";
+    }
+}
