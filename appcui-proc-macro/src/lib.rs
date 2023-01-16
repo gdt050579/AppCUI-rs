1564ec17076127aa97e96b6e1a10a0c489295266
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-01-17 01:16:14 +0200

    Support for OnPaint for generated controls

--- appcui-proc-macro/src/lib.rs
+++ appcui-proc-macro/src/lib.rs
@@ -1,19 +1,27 @@
 mod arguments;
 mod utils;
-use proc_macro::*;
 use arguments::*;
+use proc_macro::*;
 
 extern crate proc_macro;
 
-static DEREF_CODE: &str = "
-impl std::ops::Deref for $STRUCT_NAME$ {
-    type Target = $BASE$;
-    fn deref(&self) -> &Self::Target { return &self.base; }
+mod templates {
+    pub static DEREF_CODE: &str = "
+    impl std::ops::Deref for $STRUCT_NAME$ {
+        type Target = $BASE$;
+        fn deref(&self) -> &Self::Target { return &self.base; }
+    }
+    impl std::ops::DerefMut for $STRUCT_NAME$ {
+        fn deref_mut(&mut self) -> &mut Self::Target { return &mut self.base; }
+    }
+    ";
+
+    pub static ON_PAINT_CODE: &str = "
+    impl OnPaint for $STRUCT_NAME$ {
+        fn on_paint(&self)  { self.base.on_paint(); }
+    }
+    ";
 }
-impl std::ops::DerefMut for $STRUCT_NAME$ {
-    fn deref_mut(&mut self) -> &mut Self::Target { return &mut self.base; }
-}
-";
 #[allow(non_snake_case)]
 #[proc_macro_attribute]
 pub fn AppCUIControl(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -24,10 +32,15 @@ pub fn AppCUIControl(args: TokenStream, input: TokenStream) -> TokenStream {
     base_definition.push_str(", ");
     let mut code = input.to_string().replace("{", base_definition.as_str());
     let struct_name = utils::extract_structure_name(code.as_str());
-    code.push_str(DEREF_CODE);
+    code.push_str(templates::DEREF_CODE);
+    if a.on_paint {
+        code.push_str(templates::ON_PAINT_CODE);
+    }
 
     // replace templates
-    code = code.replace("$STRUCT_NAME$",&struct_name).replace("$BASE$", &a.base);
-    println!("{}",code);
+    code = code
+        .replace("$STRUCT_NAME$", &struct_name)
+        .replace("$BASE$", &a.base);
+    println!("{}", code);
     TokenStream::new()
 }
