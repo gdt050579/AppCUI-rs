bf4d6aed2300ebdfddfd43733c78cef8cc772cec
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-01-19 00:54:25 +0200

    Add surface and terminal to Application

--- appcui/src/system/app.rs
+++ appcui/src/system/app.rs
@@ -1,11 +1,23 @@
+use super::Theme;
+use crate::graphics::Surface;
+use crate::terminal::*;
 pub struct App
 {
-
+    theme: Theme,
+    terminal: Box<dyn Terminal>,
+    surface: Surface,
 }
 impl App {
-    fn new()->Self {
+    pub fn new()->Self {
+        let term = TerminalType::new(TerminalType::WindowsConsole).expect("Unable to create a terminal object !");
+        let surface = Surface::new(term.get_width(),term.get_height());
         App {
-            
+            theme: Theme::default(),
+            terminal: term,
+            surface: surface
         }
+    }
+    pub fn run(self) {
+        // must pe self so that after a run a second call will not be possible
     }
 }
