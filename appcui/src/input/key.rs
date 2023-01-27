b4596395cf097093d1a9244e67c968dd9714711e
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-01-27 23:46:20 +0200

    Key::From_char (shortcut)

--- appcui/src/input/key.rs
+++ appcui/src/input/key.rs
@@ -14,6 +14,55 @@ impl Key {
             modifier: modifier,
         }
     }
+    pub fn from_char(character: char, modifier: KeyModifier) -> Key {
+        let code = match character {
+            'a' | 'A' => KeyCode::A,
+            'b' | 'B' => KeyCode::B,
+            'c' | 'C' => KeyCode::C,
+            'd' | 'D' => KeyCode::D,
+            'e' | 'E' => KeyCode::E,
+            'f' | 'F' => KeyCode::F,
+            'g' | 'G' => KeyCode::G,
+            'h' | 'H' => KeyCode::H,
+            'i' | 'I' => KeyCode::I,
+            'j' | 'J' => KeyCode::J,
+            'k' | 'K' => KeyCode::K,
+            'l' | 'L' => KeyCode::L,
+            'm' | 'M' => KeyCode::M,
+            'n' | 'N' => KeyCode::N,
+            'o' | 'O' => KeyCode::O,
+            'p' | 'P' => KeyCode::P,
+            'q' | 'Q' => KeyCode::Q,
+            'r' | 'R' => KeyCode::R,
+            's' | 'S' => KeyCode::S,
+            't' | 'T' => KeyCode::T,
+            'u' | 'U' => KeyCode::U,
+            'v' | 'V' => KeyCode::V,
+            'w' | 'W' => KeyCode::W,
+            'x' | 'X' => KeyCode::X,
+            'y' | 'Y' => KeyCode::Y,
+            'z' | 'Z' => KeyCode::Z,
+            '0' => KeyCode::N0,
+            '1' => KeyCode::N1,
+            '2' => KeyCode::N2,
+            '3' => KeyCode::N3,
+            '4' => KeyCode::N4,
+            '5' => KeyCode::N5,
+            '6' => KeyCode::N6,
+            '7' => KeyCode::N7,
+            '8' => KeyCode::N8,
+            '9' => KeyCode::N9,
+            _ => KeyCode::None,
+        };
+        if code == KeyCode::None {
+            return Key {
+                code: KeyCode::None,
+                modifier: KeyModifier::None,
+            };
+        } else {
+            return Key { code, modifier };
+        }
+    }
 }
 
 impl Default for Key {
