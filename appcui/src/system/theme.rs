79bdcd9c4c3cc8b094e3fcb02cccb6f7430c6f1b
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-01-30 09:03:31 +0200

    Symbol colors in Theme object

--- appcui/src/system/theme.rs
+++ appcui/src/system/theme.rs
@@ -19,9 +19,23 @@ pub struct Text {
     pub enphasized_3: CharAttribute,
 }
 #[derive(Default)]
+pub struct Symbol {
+    pub inactive: CharAttribute,
+    pub hovered: CharAttribute,
+    pub pressed: CharAttribute,
+    pub checked: CharAttribute,
+    pub unchecked: CharAttribute,
+    pub unknown: CharAttribute,
+    pub arrows: CharAttribute,
+    pub close: CharAttribute,
+    pub maximized: CharAttribute,
+    pub resize: CharAttribute,
+}
+#[derive(Default)]
 pub struct Theme {
     pub desktop: Desktop,
     pub text: Text,
+    pub symbol: Symbol,
 }
 impl Theme {
     fn new()->Self {
@@ -31,6 +45,7 @@ impl Theme {
     }
     fn set_regular_theme(&mut self) {
         self.desktop.character = Character::new(SpecialChar::Block50,Color::Gray, Color::Black, CharFlags::None);
+        
         self.text.normal = CharAttribute::with_fore_color(Color::Silver);
         self.text.error = CharAttribute::with_fore_color(Color::Red);
         self.text.warning = CharAttribute::with_fore_color(Color::Olive);
@@ -42,6 +57,17 @@ impl Theme {
         self.text.enphasized_1 = CharAttribute::with_fore_color(Color::Aqua);
         self.text.enphasized_2 = CharAttribute::with_fore_color(Color::Green);
         self.text.enphasized_3 = CharAttribute::with_fore_color(Color::Pink);
+
+        self.symbol.inactive = CharAttribute::with_fore_color(Color::Gray);
+        self.symbol.hovered = CharAttribute::with_color(Color::Black, Color::Yellow);
+        self.symbol.pressed = CharAttribute::with_color(Color::Black, Color::Silver);
+        self.symbol.checked = CharAttribute::with_fore_color(Color::Green);
+        self.symbol.unchecked = CharAttribute::with_fore_color(Color::Red);
+        self.symbol.unknown = CharAttribute::with_fore_color(Color::Olive);
+        self.symbol.arrows = CharAttribute::with_fore_color(Color::Aqua);
+        self.symbol.close = CharAttribute::with_fore_color(Color::Red);
+        self.symbol.maximized = CharAttribute::with_fore_color(Color::Aqua);
+        self.symbol.resize = CharAttribute::with_fore_color(Color::Aqua);
 
     }
 }
