1a29496989b90f2415a09896ad535ec377a7f35f
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-02-18 17:45:12 +0200

    refactor terminal support + enpty/null terminal

---
+++ appcui/src/terminal/null_terminal.rs
@@ -0,0 +1,20 @@
+use super::Terminal;
+use super::SystemEvent;
+
+pub (crate) struct NullTerminal {}
+impl Terminal for NullTerminal {
+    fn update_screen(&mut self, surface: &crate::graphics::Surface) {
+    }
+
+    fn get_width(&self) -> u32 {
+        0
+    }
+
+    fn get_height(&self) -> u32 {
+        0
+    }
+
+    fn get_system_event(&mut self)-> SystemEvent {
+        SystemEvent::None
+    }
+}
