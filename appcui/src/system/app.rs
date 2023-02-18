5dc075171a86e622375fbc15a2cbbd9843ef7642
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-02-18 21:22:42 +0200

    Runtime manager object

--- appcui/src/system/app.rs
+++ appcui/src/system/app.rs
@@ -1,76 +1,28 @@
-use super::Theme;
-use crate::controls::ControlWrapper;
-use crate::controls::*;
+use super::RuntimeManager;
 use crate::controls::events::Control;
-use crate::graphics::{ClipArea, Point, Surface};
-use crate::terminal::*;
+
 
 pub struct App {
-    theme: Theme,
-    terminal: Box<dyn Terminal>,
-    surface: Surface,
-    root_control: ControlWrapper,
+    _phantom: (),
 }
 
 impl App {
     pub fn new() -> Self {
-        let term =
-            TerminalType::new(TerminalType::Debug).expect("Unable to create a terminal object !");
-        let surface = Surface::new(term.get_width(), term.get_height());
-        App {
-            theme: Theme::new(),
-            terminal: term,
-            surface: surface,
-            root_control: ControlWrapper::new(Desktop::new()),
-        }        
+        // before creating an App --> initialize Runtime Manager
+        RuntimeManager::create();
+        App { _phantom: () }
     }
     pub fn run(mut self) {
         // must pe self so that after a run a second call will not be possible
-        self.recompute_layouts();
-        self.paint();
-        let sys_event = self.terminal.get_system_event();
-        match sys_event {
-            SystemEvent::None => {},
-            SystemEvent::AppClose => todo!(),
-            SystemEvent::KeyPressed(event) => self.process_keypressed_event(event),
-            SystemEvent::KeyModifierChanged(_) => todo!(),
-            SystemEvent::Resize(_) => todo!(),
-            SystemEvent::MouseButtonDown(_) => todo!(),
-            SystemEvent::MouseButtonUp(_) => todo!(),
-            SystemEvent::MouseDoubleClick(_) => todo!(),
-            SystemEvent::MouseMove(_) => todo!(),
-            SystemEvent::MouseWheel(_) => todo!(),
-        }
-
+        RuntimeManager::get().run();
     }
 
-    pub fn add<T>(&mut self, window: T) where T: Control+'static {
-        self.root_control.get_manager_mut().children.push(ControlWrapper::new(window));
+    pub fn add<T>(&mut self, window: T)
+    where
+        T: Control + 'static,
+    {
+        RuntimeManager::get().add(window);
     }
 
-    fn recompute_layouts(&mut self) {
-        let client = ClipArea::new(
-            0,
-            0,
-            (self.terminal.get_width() as i32) - 1,
-            (self.terminal.get_height() as i32) - 1,
-        );
-        self.root_control.get_manager_mut().update_layout(
-            &client,
-            Point::default(),
-            self.terminal.get_width() as u16,
-            self.terminal.get_height() as u16,
-        );
-    }
 
-    fn paint(&mut self) {
-        self.root_control.paint(&mut self.surface, &self.theme);
-        self.terminal.update_screen(&self.surface);
-    }
-    fn process_keypressed_event(&mut self, event: KeyPressedEvent) {
-        // check if desktop can process this event
-        if self.root_control.process_keypressed_event(event.key, event.character) {
-            return;
-        }
-    }
 }
---
+++ appcui/src/system/runtime_manager.rs
@@ -0,0 +1,87 @@
+use super::Theme;
+use crate::controls::events::Control;
+use crate::controls::ControlWrapper;
+use crate::controls::*;
+use crate::graphics::{ClipArea, Point, Surface};
+use crate::terminal::*;
+
+pub(crate) struct RuntimeManager {
+    theme: Theme,
+    terminal: Box<dyn Terminal>,
+    surface: Surface,
+    controls: Vec<ControlWrapper>,
+}
+
+static mut RUNTIME_MANAGER: Option<RuntimeManager> = None;
+
+impl RuntimeManager {
+    pub(super) fn create() {
+        let term =
+            TerminalType::new(TerminalType::Debug).expect("Unable to create a terminal object !");
+        let surface = Surface::new(term.get_width(), term.get_height());
+        let mut manager = RuntimeManager {
+            theme: Theme::new(),
+            terminal: term,
+            surface: surface,
+            controls: Vec::with_capacity(128),
+        };
+        unsafe {
+            RUNTIME_MANAGER = Some(manager);
+        }
+    }
+    pub(crate) fn get() -> &'static mut RuntimeManager {
+        unsafe { RUNTIME_MANAGER.as_mut().unwrap() }
+    }
+    pub(crate) fn add<T>(&mut self, window: T)
+    where
+        T: Control + 'static,
+    {
+        let c = ControlWrapper::new(window);
+    }
+    pub (crate) fn run(&mut self) {
+        // must pe self so that after a run a second call will not be possible
+        self.recompute_layouts();
+        self.paint();
+        let sys_event = self.terminal.get_system_event();
+        match sys_event {
+            SystemEvent::None => {}
+            SystemEvent::AppClose => todo!(),
+            SystemEvent::KeyPressed(event) => self.process_keypressed_event(event),
+            SystemEvent::KeyModifierChanged(_) => todo!(),
+            SystemEvent::Resize(_) => todo!(),
+            SystemEvent::MouseButtonDown(_) => todo!(),
+            SystemEvent::MouseButtonUp(_) => todo!(),
+            SystemEvent::MouseDoubleClick(_) => todo!(),
+            SystemEvent::MouseMove(_) => todo!(),
+            SystemEvent::MouseWheel(_) => todo!(),
+        }
+    }
+    fn recompute_layouts(&mut self) {
+        let client = ClipArea::new(
+            0,
+            0,
+            (self.terminal.get_width() as i32) - 1,
+            (self.terminal.get_height() as i32) - 1,
+        );
+        self.root_control.get_manager_mut().update_layout(
+            &client,
+            Point::default(),
+            self.terminal.get_width() as u16,
+            self.terminal.get_height() as u16,
+        );
+    }
+
+    fn paint(&mut self) {
+        self.root_control.paint(&mut self.surface, &self.theme);
+        self.terminal.update_screen(&self.surface);
+    }
+    fn process_keypressed_event(&mut self, event: KeyPressedEvent) {
+        // check if desktop can process this event
+        if self
+            .root_control
+            .process_keypressed_event(event.key, event.character)
+        {
+            return;
+        }
+    }
+}
