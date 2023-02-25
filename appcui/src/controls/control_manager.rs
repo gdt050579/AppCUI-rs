953dce4670caa391630b220bc0a1b31cc6c4c934
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-02-25 17:06:47 +0200

    Refactor entire AppCUI tree (2)

--- appcui/src/controls/control_manager.rs
+++ appcui/src/controls/control_manager.rs
@@ -1,4 +1,4 @@
-use crate::graphics::{Surface, Point, ClipArea};
+use crate::graphics::{ClipArea, Point, Surface};
 use crate::input::Key;
 use crate::system::Theme;
 
@@ -13,7 +13,7 @@ pub(crate) struct ControlManager {
     interface: NonNull<dyn Control>,
     base: *mut ControlBase,
     version: u32,
-    index: u32
+    index: u32,
 }
 impl ControlManager {
     #[inline]
@@ -46,18 +46,67 @@ impl ControlManager {
             interface: ctrl,
             base: ptr as *mut ControlBase,
             version: (GLOBAL_VERSION.fetch_add(1, Ordering::SeqCst) & 0xFFFFFFFF) as u32,
-            index: ControlBase::INVALID_CONTROL_ID
+            index: ControlBase::INVALID_CONTROL_ID,
         }
     }
     pub(crate) fn get_mut<T>(&mut self) -> &mut T
     where
         T: Control + 'static,
     {
-        unsafe { &mut *(self.base as *mut T)}
+        unsafe { &mut *(self.base as *mut T) }
     }
-
-
-
+    pub(crate) fn paint(&self, surface: &mut Surface, theme: &Theme) {
+        if self.get_base().prepare_paint(surface) {
+            // paint is possible
+            self.get_control().on_paint(surface, theme);
+            for child in &self.get_base().children {
+                child.paint(surface, theme);
+            }
+        }
+    }
+    pub(crate) fn update_layout(
+        &mut self,
+        parent_clip: &ClipArea,
+        parent_origin: Point,
+        parent_width: u16,
+        parent_height: u16,
+    ) {
+        let base = self.get_base_mut();
+        base.update_control_layout_and_screen_origin(
+            parent_clip,
+            parent_origin,
+            parent_width,
+            parent_height,
+        );
+        // process the same thing for its children
+        let client_clip = base.get_client_clip();
+        let w = base.get_width();
+        let h = base.get_height();
+        let p = base.screen_origin;
+        for child in &mut base.children {
+            child.update_layout(&client_clip, p, w, h);
+        }
+    }
+    pub(crate) fn process_keypressed_event(
+        &mut self,
+        key: Key,
+        character: char,
+    ) -> EventProcessStatus {
+        let base = self.get_base_mut();
+        if base.can_receive_input() == false {
+            return EventProcessStatus::Ignored;
+        }
+        let focused_child_index = base.focused_child_index as usize;
+        if focused_child_index >= base.children.len() {
+            return EventProcessStatus::Ignored;
+        }
+        let child = &mut base.children[focused_child_index];
+        if child.process_keypressed_event(key, character) == EventProcessStatus::Processed {
+            return EventProcessStatus::Processed;
+        }
+        // else --> call it ourselves
+        return self.get_control_mut().on_key_pressed(key, character);
+    }
 }
 
 impl Drop for ControlManager {
--- appcui/src/system/runtime_manager.rs
+++ appcui/src/system/runtime_manager.rs
@@ -10,7 +10,7 @@ pub(crate) struct RuntimeManager {
     theme: Theme,
     terminal: Box<dyn Terminal>,
     surface: Surface,
-    controls: Vec<Option<ControlManager>>,
+    root: ControlManager,
 }
 
 static mut RUNTIME_MANAGER: Option<RuntimeManager> = None;
@@ -24,7 +24,7 @@ impl RuntimeManager {
             theme: Theme::new(),
             terminal: term,
             surface: surface,
-            controls: Vec::with_capacity(128),
+            root: ControlManager::new(Desktop::new()),
         };
         unsafe {
             RUNTIME_MANAGER = Some(manager);
@@ -58,46 +58,7 @@ impl RuntimeManager {
             SystemEvent::MouseWheel(_) => todo!(),
         }
     }
-    fn get_desktop(&mut self) -> &mut ControlManager {
-        return self.controls[0].as_mut().unwrap();
-    }
-    fn get_control_mut(&mut self, index: usize) -> Option<&mut ControlManager> {
-        if index >= self.controls.len() {
-            return None;
-        }
-        let c = &mut self.controls[index];
-        if c.is_none() {
-            return None;
-        }
-        return c.as_mut();
-    }
-    fn recompute_control_layouts(
-        &mut self,
-        parent_clip: &ClipArea,
-        parent_origin: Point,
-        parent_width: u16,
-        parent_height: u16,
-        index: usize,
-    ) {
-        if let Some(c) = self.get_control_mut(index) {
-            let base = c.get_base_mut();
-            base.update_control_layout_and_screen_origin(
-                parent_clip,
-                parent_origin,
-                parent_width,
-                parent_height,
-            );
-            // process the same thing for its children
-            let base = c.get_base();
-            let client_clip = base.get_client_clip();
-            let w = base.get_width();
-            let h = base.get_height();
-            let p = base.screen_origin;
-            for c_idx in &base.children {
-                self.recompute_control_layouts(&client_clip, p, w, h, *c_idx as usize);
-            }
-        }
-    }
+
     fn recompute_layouts(&mut self) {
         let client = ClipArea::new(
             0,
@@ -105,62 +66,20 @@ impl RuntimeManager {
             (self.terminal.get_width() as i32) - 1,
             (self.terminal.get_height() as i32) - 1,
         );
-        self.recompute_control_layouts(
+        self.root.update_layout(
             &client,
             Point::default(),
             self.terminal.get_width() as u16,
             self.terminal.get_height() as u16,
-            0,
         );
     }
 
-    fn paint_control(&mut self, index: usize) {
-        if let Some(c) = self.get_control_mut(index) {
-            c.get_base().prepare_paint(&mut self.surface);
-            c.get_control().on_paint(&mut self.surface, &self.theme);
-            for c_idx in c.get_base().children {
-                self.paint_control(c_idx as usize);
-            }
-        }
-    }
     fn paint(&mut self) {
-        self.paint_control(0); // desktop
+        self.root.paint(&mut self.surface, &self.theme);
         self.terminal.update_screen(&self.surface);
     }
-    fn process_control_keypressed_event(
-        &mut self,
-        key: Key,
-        character: char,
-        index: usize,
-    ) -> EventProcessStatus {
-        let mut run_through_interface = false;
-        if let Some(c) = self.get_control_mut(index) {
-            let base = c.get_base_mut();
-            if base.can_receive_input() == false {
-                return EventProcessStatus::Ignored;
-            }
-            let focused_child_index = base.focused_child_index as usize;
-            if focused_child_index >= base.children.len() {
-                return EventProcessStatus::Ignored;
-            }
-            let child_index = base.children[focused_child_index] as usize;
-            if self.process_control_keypressed_event(key, character, child_index)
-                == EventProcessStatus::Processed
-            {
-                return EventProcessStatus::Processed;
-            }
-            // else --> call it ourselves
-            run_through_interface = true;
-        }
-        if run_through_interface {
-            self.get_control_mut(index)
-                .unwrap()
-                .get_control_mut()
-                .on_key_pressed(key, character);
-        }
-        return EventProcessStatus::Ignored;
-    }
+
     fn process_keypressed_event(&mut self, event: KeyPressedEvent) {
-        self.process_control_keypressed_event(event.key, event.character, 0);
+        self.root.process_keypressed_event(event.key, event.character);
     }
 }
