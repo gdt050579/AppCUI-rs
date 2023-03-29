744fc272037586b62313c26695c2ebcb0ad31610
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-03-29 20:43:28 +0300

    key! macro

--- appcui-proc-macro/src/lib.rs
+++ appcui-proc-macro/src/lib.rs
@@ -32,20 +32,20 @@ mod templates {
     impl OnKeyPressed for $STRUCT_NAME$ {
         fn on_key_pressed(&mut self, key: Key, character: char)->EventProcessStatus { return self.base.on_key_pressed(key, character); }
     }
-    ";   
-    
+    ";
+
     pub static ON_MOUSE_EVENT_TRAIT: &str = "
     impl OnMouseEvent for $STRUCT_NAME$ {
         fn on_mouse_event(&mut self, event: &MouseEvent)->EventProcessStatus { return self.base.on_mouse_event(event); }
     }
-    "; 
+    ";
 
     pub static ON_DEFAULT_ACTION_TRAIT: &str = "
     impl OnDefaultAction for $STRUCT_NAME$ {
         fn on_default_action(&mut self){ self.base.on_default_action(); }
     }
-    "; 
-    
+    ";
+
     pub static ON_RESIZE_TRAIT: &str = "
     impl OnResize for $STRUCT_NAME$ {
         fn on_resize(&mut self, old: Size, new: Size)  { self.base.on_resize(old, new); }
@@ -87,4 +87,122 @@ pub fn AppCUIControl(args: TokenStream, input: TokenStream) -> TokenStream {
         .replace("$BASE$", &a.base);
     println!("{}", code);
     TokenStream::from_str(&code).expect("Fail to convert string to token stream")
+}
+
+#[proc_macro]
+pub fn key(input: TokenStream) -> TokenStream {
+    let mut tokens = input.into_iter().peekable();
+
+    let string_param = match tokens.next() {
+        Some(TokenTree::Literal(lit)) => lit.to_string(),
+        _ => panic!("The parameter provided to the key macro must be a string literal."),
+    };
+
+    if tokens.peek().is_some() {
+        panic!("Exactly one string must be provided as input.");
+    }
+
+    let value = parse_string_key_representation(&string_param);
+    TokenStream::from_str(value.to_string().as_str()).expect("Fail to convert key to token stream")
+}
+
+fn parse_string_key_representation(string: &str) -> u16 {
+    let mut key_value = 0u16;
+    let mut modifiers = 0u16;
+
+    for key in string.split('+') {
+        let modifier = match key {
+            "Ctrl" => 0x100,
+            "Alt" => 0x200,
+            "Shift" => 0x400,
+            _ => 0,
+        };
+
+        let key_code = if modifier != 0 {
+            0
+        } else {
+            parse_key_name(key)
+        };
+        if (modifier==0) && (key_code==0) {
+            panic!("Unknown key or modifier: {}", key);
+        }
+        if (modifiers & modifier) != 0 {
+            panic!("Duplicate modifier: {}", key);
+        }
+        if (key_value!=0) && (key_code!=0) {
+            panic!("A key can only be added once: {}", key);
+        }
+        modifiers |= modifier;
+        key_value = key_code;
+    }
+    0
+}
+
+fn parse_key_name(key: &str) -> u16 {
+    match key {
+    "F1" => 1,
+    "F2" => 2,
+    "F3" => 3,
+    "F4" => 4,
+    "F5" => 5,
+    "F6" => 6,
+    "F7" => 7,
+    "F8" => 8,
+    "F9" => 9,
+    "F10" => 10,
+    "F11" => 11,
+    "F12" => 12,
+    "Enter" => 13,
+    "Escape" => 14,
+    "Insert" => 15,
+    "Delete" => 16,
+    "Backspace" => 17,
+    "Tab" => 18,
+    "Left" => 19,
+    "Up" => 20,
+    "Down" => 21,
+    "Right" => 22,
+    "PageUp" => 23,
+    "PageDown" => 24,
+    "Home" => 25,
+    "End" => 26,
+    "Space" => 27,
+    "A" => 28,
+    "B" => 29,
+    "C" => 30,
+    "D" => 31,
+    "E" => 32,
+    "F" => 33,
+    "G" => 34,
+    "H" => 35,
+    "I" => 36,
+    "J" => 37,
+    "K" => 38,
+    "L" => 39,
+    "M" => 40,
+    "N" => 41,
+    "O" => 42,
+    "P" => 43,
+    "Q" => 44,
+    "R" => 45,
+    "S" => 46,
+    "T" => 47,
+    "U" => 48,
+    "V" => 49,
+    "W" => 50,
+    "X" => 51,
+    "Y" => 52,
+    "Z" => 53,
+    "0" => 54,
+    "1" => 55,
+    "2" => 56,
+    "3" => 57,
+    "4" => 58,
+    "5" => 59,
+    "6" => 60,
+    "7" => 61,
+    "8" => 62,
+    "9" => 63,
+    _ => 0,
+    }
 }
