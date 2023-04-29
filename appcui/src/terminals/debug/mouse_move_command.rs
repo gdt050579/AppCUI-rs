a88a637126dfa40e42789fc581c981653c9dfd40
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-04-29 13:50:36 +0300

    Mouse.Wheel command (2)

---
+++ appcui/src/terminals/debug/mouse_move_command.rs
@@ -0,0 +1,28 @@
+use crate::input::MouseButton;
+
+use super::command_parser::{CommandParser, ParserError};
+
+pub(super) struct MouseMoveCommand {
+    x: i32,
+    y: i32,
+}
+
+impl MouseMoveCommand {
+    pub(super) fn new(parser: &CommandParser)->Result<Self,ParserError> {
+        if parser.get_params_count()!=2 {
+            return Err(ParserError::new("Mouse.Move command requires 2 parameters"));
+        }
+        let x = parser.get_i32(0);
+        let y = parser.get_i32(1);
+        if x.is_none() {
+            return Err(ParserError::new("First parameter for Mouse.Move command should an integer (x value)"));
+        }
+        if y.is_none() {
+            return Err(ParserError::new("Second parameter for Mouse.Move command should an integer (y value)"));
+        }
+        Ok(Self {
+            x: x.unwrap(),
+            y: y.unwrap(),
+        })
+    }
+}
