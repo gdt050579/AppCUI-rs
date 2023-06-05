b1bbc105c9794d3e2699ce8228e117fbd0051e11
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-06-05 08:31:15 +0300

    Custom desktop tests (for painting)

--- appcui/src/controls/desktop/test.rs
+++ appcui/src/controls/desktop/test.rs
@@ -1,28 +1,37 @@
-use AppCUIProcMacro::AppCUIDesktop;
-use crate::controls::*;
 use crate::controls::events::*;
-use crate::input::*;
-use crate::graphics::*;
-use crate::system::*;
 use crate::controls::menu::*;
+use crate::controls::*;
+use crate::graphics::*;
+use crate::input::*;
+use crate::system::*;
+use AppCUIProcMacro::AppCUIDesktop;
 
 #[AppCUIDesktop(overwrite = OnPaint)]
 struct MyDesktop {}
 impl MyDesktop {
     fn new() -> Self {
         Self {
-            base: Desktop::new()
+            base: Desktop::new(),
         }
     }
 }
 impl OnPaint for MyDesktop {
     fn on_paint(&self, surface: &mut Surface, _theme: &Theme) {
-        surface.clear(Character::new('x',Color::Red,Color::Green,CharFlags::None));
+        surface.clear(Character::new(
+            'x',
+            Color::Red,
+            Color::Green,
+            CharFlags::None,
+        ));
     }
 }
 
 #[test]
 fn check_custom_paint_for_desktop() {
-    let a = App::debug(60, 10, InitializationFlags::None, MyDesktop::new(), "Paint(desktop)").unwrap();
+    let script = "
+        //Paint('desktop with red and green')
+        CheckHash(0xD490E8FF2EC89965)
+    ";
+    let a = App::debug(60, 10, InitializationFlags::None, MyDesktop::new(), script).unwrap();
     a.run();
 }
