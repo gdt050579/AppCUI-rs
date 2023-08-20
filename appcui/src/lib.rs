323b45d9b9b14b1194fae297d73de3ee9ae6b2c9
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-08-20 19:17:38 +0300

    Documentation (1) for appcui

--- appcui/src/lib.rs
+++ appcui/src/lib.rs
@@ -1,3 +1,13 @@
+//! # appcui
+//!
+//! `appcui` is a cross-platform TUI (**T**ext **U**ser **I**nterface / **T**erminal **U**ser **I**nterface) / CUI (**C**onsole **U**ser **I**nterface) framework designed to allow quick creation TUI/CUI based applications. 
+//! AppCUI has a lot of out-of-the-box controls, and can also provide quick macros to create custom controls. The list of current supported controls are:
+//! * Button
+//! * Checkbox 
+//!
+//! # Examples
+
+
 pub mod graphics;
 pub mod terminals;
 pub mod input;
