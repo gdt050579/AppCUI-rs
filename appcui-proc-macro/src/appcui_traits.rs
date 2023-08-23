786b4cbd2485a4233c08e5307d30f5b8a67c0e5d
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-08-23 09:01:22 +0300

    Refactor AppCUIProcMacro (1)

---
+++ appcui-proc-macro/src/appcui_traits.rs
@@ -0,0 +1,39 @@
+use super::templates;
+
+#[repr(u8)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+pub(crate) enum AppCUITraits {
+    Deref = 0,
+    OnPaint = 1,
+    OnKeyPressed = 2,
+    OnMouseEvents = 3,
+    OnDefaultAction = 4,
+    OnResize = 5,
+    OnFocus = 6,
+}
+
+impl AppCUITraits {
+    pub(crate) fn get_name(&self) -> &'static str {
+        match self {
+            AppCUITraits::Deref => "Deref",
+            AppCUITraits::OnPaint => "OnPaint",
+            AppCUITraits::OnKeyPressed => "OnKeyPressed",
+            AppCUITraits::OnMouseEvents => "OnMouseEvents",
+            AppCUITraits::OnDefaultAction => "OnDefaultAction",
+            AppCUITraits::OnResize => "OnResize",
+            AppCUITraits::OnFocus => "OnFocus",
+        }
+    }
+    pub(crate) fn get_basefallback_implementation(&self) -> &'static str {
+        match self {
+            AppCUITraits::Deref => templates::DEREF_TRAIT,
+            AppCUITraits::OnPaint => templates::ON_PAINT_TRAIT,
+            AppCUITraits::OnKeyPressed => templates::ON_KEY_PRESSED_TRAIT,
+            AppCUITraits::OnMouseEvents => templates::ON_MOUSE_EVENT_TRAIT,
+            AppCUITraits::OnDefaultAction => templates::ON_DEFAULT_ACTION_TRAIT,
+            AppCUITraits::OnResize => templates::ON_RESIZE_TRAIT,
+            AppCUITraits::OnFocus => templates::ON_FOCUS_TRAIT,
+        }
+
+    }
+}
---
+++ appcui-proc-macro/src/templates.rs
@@ -0,0 +1,65 @@
+pub(crate) static IMPORTS: &str = "
+use $(ROOT)::ui::*;
+use $(ROOT)::ui::common::traits::*;
+use $(ROOT)::ui::button::events::ButtonEvents;
+use $(ROOT)::ui::checkbox::events::CheckBoxEvents;
+use $(ROOT)::ui::window::events::WindowEvents;
+use $(ROOT)::ui::command_bar::events::CommandBarEvents;
+use $(ROOT)::ui::menu::events::MenuEvents;
+use $(ROOT)::ui::menu::*;
+use $(ROOT)::graphics::*;
+use $(ROOT)::system::*;
+use $(ROOT)::input::*;
+";
+
+pub(crate) static IMPORTS_INTERNAL: &str = "
+use crate::utils::*;
+use crate::ui::common::*;
+";
+
+pub(crate) static DEREF_TRAIT: &str = "
+impl std::ops::Deref for $(STRUCT_NAME) {
+    type Target = $(BASE);
+    fn deref(&self) -> &Self::Target { return &self.base; }
+}
+impl std::ops::DerefMut for $(STRUCT_NAME) {
+    fn deref_mut(&mut self) -> &mut Self::Target { return &mut self.base; }
+}
+";
+
+pub(crate) static ON_PAINT_TRAIT: &str = "
+impl OnPaint for $(STRUCT_NAME) {
+    fn on_paint(&self, surface: &mut Surface, theme: &Theme)  { self.base.on_paint(surface, theme); }
+}
+";
+
+pub(crate) static ON_KEY_PRESSED_TRAIT: &str = "
+impl OnKeyPressed for $(STRUCT_NAME) {
+    fn on_key_pressed(&mut self, key: Key, character: char)->EventProcessStatus { return self.base.on_key_pressed(key, character); }
+}
+";
+
+pub(crate) static ON_MOUSE_EVENT_TRAIT: &str = "
+impl OnMouseEvent for $(STRUCT_NAME) {
+    fn on_mouse_event(&mut self, event: &MouseEvent)->EventProcessStatus { return self.base.on_mouse_event(event); }
+}
+";
+
+pub(crate) static ON_DEFAULT_ACTION_TRAIT: &str = "
+impl OnDefaultAction for $(STRUCT_NAME) {
+    fn on_default_action(&mut self){ self.base.on_default_action(); }
+}
+";
+
+pub(crate) static ON_RESIZE_TRAIT: &str = "
+impl OnResize for $(STRUCT_NAME) {
+    fn on_resize(&mut self, old: Size, new: Size)  { self.base.on_resize(old, new); }
+}
+";
+
+pub(crate) static ON_FOCUS_TRAIT: &str = "
+impl OnFocus for $(STRUCT_NAME) {
+    fn on_focus(&mut self)  { self.base.on_focus(); }
+    fn on_lose_focus(&mut self)  { self.base.on_lose_focus(); }
+}
+";
