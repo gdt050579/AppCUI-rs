0a34c76f8f5e45ff8f5018b7d5b67d10b20efa3f
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-09-09 08:58:30 +0300

    Fix label unit tests

--- appcui/src/ui/label/tests.rs
+++ appcui/src/ui/label/tests.rs
@@ -9,15 +9,15 @@ fn check_label_position() {
     ";
     let mut a = App::debug(60, 11, script).build().unwrap();
     let mut w = Window::new("Title", Layout::new("d:c,w:40,h:9"), window::Flags::None);
-    w.add(Label::new("TopLeft", Layout::new("d:tl,w:7")));
-    w.add(Label::new("Top", Layout::new("d:t,w:3")));
-    w.add(Label::new("TopRight", Layout::new("d:tr,w:8")));
-    w.add(Label::new("Right", Layout::new("d:r,w:5")));
-    w.add(Label::new("BottomRight", Layout::new("d:br,w:11")));
-    w.add(Label::new("Bottom", Layout::new("d:b,w:6")));
-    w.add(Label::new("BottomLeft", Layout::new("d:bl,w:10")));
-    w.add(Label::new("Left", Layout::new("d:l,w:4")));
-    w.add(Label::new("Center", Layout::new("d:c,w:6")));
+    w.add(Label::new("TopLeft", Layout::new("d:tl,w:7,h:1")));
+    w.add(Label::new("Top", Layout::new("d:t,w:3,h:1")));
+    w.add(Label::new("TopRight", Layout::new("d:tr,w:8,h:1")));
+    w.add(Label::new("Right", Layout::new("d:r,w:5,h:1")));
+    w.add(Label::new("BottomRight", Layout::new("d:br,w:11,h:1")));
+    w.add(Label::new("Bottom", Layout::new("d:b,w:6,h:1")));
+    w.add(Label::new("BottomLeft", Layout::new("d:bl,w:10,h:1")));
+    w.add(Label::new("Left", Layout::new("d:l,w:4,h:1")));
+    w.add(Label::new("Center", Layout::new("d:c,w:6,h:1")));
 
     a.add_window(w);
     a.run();
