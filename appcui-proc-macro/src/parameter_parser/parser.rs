5262bc89154dfc270b3fdc97f4817722ae17eb5f
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-09-22 10:16:01 +0300

    Partial parser implementation

--- appcui-proc-macro/src/parameter_parser/parser.rs
+++ appcui-proc-macro/src/parameter_parser/parser.rs
@@ -3,35 +3,81 @@ use std::collections::HashMap;
 
 pub(crate) struct NamedParamsMap<'a> {
     named: HashMap<u64, Value<'a>>,
+    ordered: Vec<Value<'a>>,
 }
 pub(crate) enum ValueType<'a> {
+    Bool(bool),
     Integer(i32),
     String(&'a str),
     Percentage(i32),
     List(Vec<Value<'a>>),
     Dict(NamedParamsMap<'a>),
 }
+impl<'a> ValueType<'a> {
+    fn new(text: &'a str) -> Self {
+        ValueType::String(text)
+    }
+}
+
 pub(crate) struct Value<'a> {
     raw_data: &'a str,
     data_type: ValueType<'a>,
 }
+fn parse_vec<'a>(text: &'a str, tokenizer: &Tokenizer, index_start: usize, index_end: usize) -> Result<Vec<Value<'a>>, Error> {
+    todo!("not implemeted yet")
+}
 
-fn parse_dict<'a>(text: &str, tokenizer: &Tokenizer, index_start: usize, index_end: usize) -> Result<NamedParamsMap<'a>, Error> {
-    let r = NamedParamsMap {
+fn parse_dict<'a>(text: &'a str, tokenizer: &Tokenizer, index_start: usize, index_end: usize) -> Result<NamedParamsMap<'a>, Error> {
+    let mut r = NamedParamsMap {
         named: HashMap::with_capacity(8),
+        ordered: Vec::with_capacity(8),
     };
     let mut pos = index_start;
     let mut allow_value = true;
     while pos < index_end {
         let format = tokenizer.analyze(text, pos, index_end, allow_value, true)?;
-
+        if format.is_key_value() {
+            let key_token = tokenizer.get(pos);
+            let key = hash::compute(key_token.get_text(text));
+            if r.named.contains_key(&key) {
+                return Err(Error::with_token(
+                    text,
+                    format!("Duplicate parameter: '{}'", key_token.get_text(text)).as_str(),
+                    &key_token,
+                ));
+            }
+            let next = tokenizer.get(pos + 2).get_next(pos + 2);
+            let data_type = match tokenizer.get(pos + 2).get_type() {
+                TokenType::OpenBrace => ValueType::Dict(parse_dict(text, tokenizer, pos + 3, next - 1)?),
+                TokenType::OpenSquareBracket => ValueType::List(parse_vec(text, tokenizer, pos + 3, next - 1)?),
+                _ => ValueType::new(tokenizer.get(pos + 2).get_text(text)),
+            };
+            r.named.insert(
+                key,
+                Value {
+                    raw_data: key_token.get_text(text),
+                    data_type,
+                },
+            );
+        } else {
+            let next = tokenizer.get(pos).get_next(pos);
+            let data_type = match tokenizer.get(pos).get_type() {
+                TokenType::OpenBrace => ValueType::Dict(parse_dict(text, tokenizer, pos + 1, next - 1)?),
+                TokenType::OpenSquareBracket => ValueType::List(parse_vec(text, tokenizer, pos + 1, next - 1)?),
+                _ => ValueType::new(tokenizer.get(pos).get_text(text)),
+            };
+            r.ordered.push(Value {
+                raw_data: tokenizer.get(pos).get_text(text),
+                data_type,
+            });
+        }
         // first time a key:value is found, seding values in order is not possible anymore
         allow_value &= !format.is_key_value();
         pos = format.get_next_pos();
     }
     Ok(r)
 }
-pub(crate) fn parse<'a>(text: &str) -> Result<NamedParamsMap<'a>, Error> {
+pub(crate) fn parse<'a>(text: &'a str) -> Result<NamedParamsMap<'a>, Error> {
     let t = Tokenizer::new(text)?;
     let r = parse_dict(text, &t, 0, t.count())?;
     Ok(r)
