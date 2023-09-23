7722da068a029a4214b4374c3e858f95b8e04d27
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-09-23 10:31:12 +0300

    partial unit test for a button macro

--- appcui/src/ui/button/tests.rs
+++ appcui/src/ui/button/tests.rs
@@ -132,3 +132,17 @@ fn check_button_control_2() {
     a.add_window(MyWin::new());
     a.run();
 }
+
+#[test]
+fn check_button_control_with_macro() {
+    let script = "
+        Paint.Enable(false)
+        Paint('Initial focus => Add (0) and Reset')   
+        //CheckHash(0xBAE7A3089D249D76)
+    ";
+    let mut a = App::debug(60, 10, script).build().unwrap();
+    let mut w = Window::new("Macro Test",Layout::new("d:c:w,40,h:8"),window::Flags::None);
+    w.add(button!("text=Start,x:2,y:3,w:15"));
+    a.add_window(w);
+    a.run();
+}
