188630c094448e3293f6dbc8b9204c198f0536bc
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-11-23 09:06:14 +0200

    Checkbox example

---
+++ examples/checkboxes.rs
@@ -0,0 +1,52 @@
+use appcui::prelude::*;
+
+#[Window(events = CheckBoxEvents)]
+struct MyWin {
+    c1: Handle<CheckBox>,
+    c2: Handle<CheckBox>,
+    c3: Handle<CheckBox>,
+    lb: Handle<Label>,
+}
+
+impl MyWin {
+    fn new() -> Self {
+        let mut win = MyWin {
+            base: window!("Checkboxes,d:c,w:40,h:12"),
+            c1: Handle::None,
+            c2: Handle::None,
+            c3: Handle::None,
+            lb: Handle::None,
+        };
+        win.c1 = win.add(checkbox!("'&Single line',x:2,y:2,w:15"));
+        win.c2 = win.add(checkbox!("Caption='Inactive checkbox',x:2,y:3,w:15,enabled:false"));
+        win.c3 = win.add(checkbox!("Text='A multi line checkbox',x:2,y:4,w:20,h:3,checked:true"));
+        win.lb = win.add(Label::new("", Layout::new("x:2,y:0,w:35")));
+        win
+    }
+    fn set_label_text(&mut self, txt: &str) {
+        let h = self.lb;
+        if let Some(label) = self.get_control_mut(h) {
+            label.set_caption(txt);
+        }
+    }
+}
+
+impl CheckBoxEvents for MyWin {
+    fn on_status_changed(&mut self, handle: Handle<CheckBox>, checked: bool) -> EventProcessStatus {
+        let id = match () {
+            _ if handle == self.c1 => 1,
+            _ if handle == self.c2 => 2,
+            _ if handle == self.c3 => 3,
+            _ => 0,
+        };
+        self.set_label_text(format!("Checkbox {} check status: {}", id, checked).as_str());
+        EventProcessStatus::Ignored
+    }
+}
+
+fn main() -> Result<(), appcui::system::Error> {
+    let mut app = App::new().build()?;
+    app.add_window(MyWin::new());
+    app.run();
+    Ok(())
+}
