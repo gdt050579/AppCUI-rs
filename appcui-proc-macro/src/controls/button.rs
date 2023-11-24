4e932aaf27613461e509369e3fe7a6cc7a2d6067
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-11-24 12:42:53 +0200

    Switch label! to the new format

--- appcui-proc-macro/src/controls/button.rs
+++ appcui-proc-macro/src/controls/button.rs
@@ -22,34 +22,4 @@ pub(crate) fn create(input: TokenStream) -> TokenStream {
     cb.add_basecontrol_operations();
     cb.into()
 }
-// pub(crate) fn create_old(input: TokenStream) -> TokenStream {
-//     let s = utils::token_stream_to_string("button", input);
-//     let mut p = parameter_parser::parse(&s).unwrap();
-//     p.validate_positional_parameters(&s, POSILITIONAL_PARAMETERS).unwrap();
-//     p.validate_names_parameters(&s, NAMED_PARAMETERS).unwrap();
-//     p.validate_names_parameters(&s, common::CONTROL_NAMED_PARAMATERS).unwrap();
-//     p.check_unkwnon_params(&s).unwrap();
-//     // all good --> lets build the query
-//     let mut result = String::with_capacity(512);
-//     result.push_str("{\n\t");
-//     result.push_str("let mut but = Button::new(");
-//     // first add the caption
-//     let caption = p.get("caption").expect("First parameter (caption) has to be provided !");
-//     common::add_string(&mut result, caption.get_string());
-//     // second add the layout
-//     result.push_str(" , ");
-//     layout::add_layout(&mut result, &p);
-//     // lastly add the flags
-//     result.push_str(" , ");
-//     if let Some(flags) = p.get_mut("flags") {
-//         common::add_flags(&s, &mut result, "button::Flags", flags.get_list().unwrap(), unsafe { &mut BUTTON_FLAGS }).unwrap();
-//     } else {
-//         result.push_str("button::Flags::None");
-//     }
-//     result.push_str(");\n\t");
-//     // basic controls
-//     common::add_basecontrol_operations(&mut result, "but", &mut p);
-//     // finally close block
-//     result.push_str("but\n}");
-//     utils::to_token_stream(result)
-// }
+
--- appcui-proc-macro/src/controls/label.rs
+++ appcui-proc-macro/src/controls/label.rs
@@ -1,7 +1,4 @@
-use super::common;
-use super::layout;
-use super::utils;
-use crate::parameter_parser;
+use super::control_builder::ControlBuilder;
 use crate::parameter_parser::*;
 use proc_macro::*;
 
@@ -13,27 +10,13 @@ static NAMED_PARAMETERS: &[NamedParameter] = &[
     NamedParameter::new("text", "caption", ParamType::String),
 ];
 
+
 pub(crate) fn create(input: TokenStream) -> TokenStream {
-    let s = utils::token_stream_to_string("label", input);
-    let mut p = parameter_parser::parse(&s).unwrap();
-    p.validate_positional_parameters(&s, POSILITIONAL_PARAMETERS).unwrap();
-    p.validate_names_parameters(&s, NAMED_PARAMETERS).unwrap();
-    p.validate_names_parameters(&s, common::CONTROL_NAMED_PARAMATERS).unwrap();
-    p.check_unkwnon_params(&s).unwrap();
-    // all good --> lets build the query
-    let mut result = String::with_capacity(512);
-    result.push_str("{\n\t");
-    result.push_str("let mut label = Label::new(");
-    // first add the caption
-    let caption = p.get("caption").expect("First parameter (caption) has to be provided !");
-    common::add_string(&mut result, caption.get_string());
-    // second add the layout
-    result.push_str(" , ");
-    layout::add_layout(&mut result, &p);
-    result.push_str(");\n\t");
-    // basic controls
-    common::add_basecontrol_operations(&mut result, "label", &mut p);
-    // finally close block
-    result.push_str("label\n}");
-    utils::to_token_stream(result)
+    let mut cb = ControlBuilder::new("label", input, POSILITIONAL_PARAMETERS, NAMED_PARAMETERS);
+    cb.init_control("Label::new");
+    cb.add_strng_parameter("caption");  
+    cb.add_layout();
+    cb.finish_control_initialization();
+    cb.add_basecontrol_operations();
+    cb.into()
 }
