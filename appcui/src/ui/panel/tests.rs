151fd0e956d9cbfccdd472fbcbbc98dc5c2aac0b
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-11-24 23:20:29 +0200

    Unit test for page panel

--- appcui/src/ui/panel/tests.rs
+++ appcui/src/ui/panel/tests.rs
@@ -38,3 +38,22 @@ fn check_panel_with_macro() {
     a.add_window(w);
     a.run();
 }
+#[test]
+fn check_panel_with_page() {
+    let script = "
+        Paint.Enable(false)
+        Paint('Page panel')   
+        CheckHash(0xBF28919C860D0024)   
+    ";
+    let mut a = App::debug(80, 13, script).build().unwrap();
+    let mut w = window!("Title,d:c,w:78,h:11");
+    w.add(Panel::new("Options", Layout::new("x:1,y:1,w:16,h:4"), panel::Type::Page));
+    w.add(Panel::new("Options", Layout::new("x:18,y:1,w:12,h:4"), panel::Type::Page));
+    w.add(Panel::new("Options", Layout::new("x:31,y:1,w:13,h:4"), panel::Type::Page));
+    w.add(Panel::new("Options", Layout::new("x:50,y:1,w:10,h:4"), panel::Type::Page));
+    let mut inactive_panel = Panel::new("Inactive panel",Layout::new("x:1,y:5,w:74,h:4"),panel::Type::Page);
+    inactive_panel.set_enabled(false);
+    w.add(inactive_panel);
+    a.add_window(w);
+    a.run();
+}
