a7aacc024ef7c984f9e4073d08daa8735a356410
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2023-12-18 08:25:47 +0200

    Docs: toolbar item (button)

--- docs/chapter-3/event-loop/toolbar-items/button.md
+++ docs/chapter-3/event-loop/toolbar-items/button.md
@@ -1,1 +1,111 @@
 # Button toolbar item
+
+A toolbar button is a item that can be positioned on the top or bottom part of a windows (like in the following image).
+
+<img src="img/button.png" width=400/>
+
+To create a button within a toolbar use the `toolbar::Button::new(...)` method:
+
+```rust
+let toolbar_button = toolbar::Button::new("content");
+```
+
+or the `toolbaritem!` macro:
+
+```rust
+let toolbar_button_1 = toolbaritem!("content,type=button");
+let toolbal_button_2 = toolbaritem!("content='Start',type:button");
+let toolbal_button_3 = toolbaritem!("content='&Stop',type:button,tooltip:'a tooltip'");
+let toolbal_button_4 = toolbaritem!("content='hidden button',type:button,visible:false");
+```
+
+
+Using the character `&` as part of the button caption will associate the next character (if it is a letter or number) as a hot-key for the button. For example, the following caption `St&art` will set `Alt+A` as a hot-key for the button.
+
+
+The following parameters are supported for a toolbar button:
+
+| Parameter name      | Type   | Positional parameter                | Purpose |
+|---------------------|--------|-------------------------------------|---------|
+| `text` or `caption` | String | **Yes** (first postional parameter) | The caption (text) written on the button |
+| `type`              | String | **No**                              | For a button use: `type:Button` |
+| `tooltip`           | String | **No**                              | The tooltip associated with the button |
+| `visible`           | Bool   | **No**                              | `true` if the toolbar item is visible (this is also the default setting) or `false` otherwise |
+
+Besides the default methods that every toolbar item has (as described [here](../toolbar.md#common-methods)), the following methods are available for a toolbar label:
+
+| Method             | Purpose                                                                                                          |
+|--------------------|------------------------------------------------------------------------------------------------------------------|
+| `set_caption(...)` | Set the new caption for a button. The width (in characters) of the button is the considered to be the number of characters in its content |
+| `get_caption()`    | Returns the current caption of a button. |
+
+## Events
+
+To intercept button clicks, implement `ToolBarEvents` for the current window, as presented in the following example:
+```rust,no_run
+#[Window(events=ToolBarEvents)]
+struct MyWin { /* data members */ }
+
+impl ToolBarEvens for MyWin {
+    fn on_button_clicked(&mut self, handle: Handle<toolbar::Button>) -> EventProcessStatus {
+        // process click events from a toolbar button
+    }
+}
+```
+
+## Example
+
+The following example creates two buttons on the bottom right part of a window toolbar that can be used to increase the value of a number.
+
+```rust,no_run
+#[Window(events = ToolBarEvents)]
+struct MyWin {
+    increase_button: Handle<toolbar::Button>,
+    decrease_button: Handle<toolbar::Button>,
+    text: Handle<Label>,
+    number: u32,
+}
+
+impl MyWin {
+    fn new() -> Self {
+        let mut win = MyWin {
+            base: window!("'My Win',d:c,w:40,h:6"),
+            increase_button: Handle::None,
+            decrease_button: Handle::None,
+            text: Handle::None,
+            number: 10,
+        };
+        // create a group
+        let g = win.get_toolbar().create_group(toolbar::GroupPosition::BottomRight);
+        // add buttons
+        win.increase_button = win.get_toolbar().add(g, toolbar::Button::new("+"));
+        win.decrease_button = win.get_toolbar().add(g, toolbar::Button::new("-"));
+        // add a label
+        win.text = win.add(label!("10,d:c,w:2,h:1"));
+        win
+    }
+}
+impl ToolBarEvents for MyWin {
+    fn on_button_clicked(&mut self, handle: Handle<toolbar::Button>) -> EventProcessStatus {
+        match () {
+            _ if handle == self.increase_button => self.number += 1,
+            _ if handle == self.decrease_button => self.number -= 1,
+            _ => {}
+        }
+        let h = self.text;
+        let n = self.number;
+        if let Some(label) = self.get_control_mut(h) {            
+            label.set_caption(format!("{}", n).as_str());
+        }
+        EventProcessStatus::Processed
+    }
+}
+
+fn main() -> Result<(), appcui::system::Error> {
+    let mut a = App::new().build()?;
+    a.add_window(MyWin::new());
+    a.run();
+    Ok(())
+}
+
+```
--- examples/simple_window.rs
+++ examples/simple_window.rs
@@ -237,15 +237,11 @@ use appcui::prelude::*;
 // //     Ok(())
 // // }use appcui::prelude::*;
 
-#[Window(events = ButtonEvents+CheckBoxEvents)]
+#[Window(events = ToolBarEvents)]
 struct MyWin {
-    increase_button: Handle<Button>,
-    dec: Handle<toolbar::Label>,
-    hex: Handle<toolbar::Label>,
-    bin: Handle<toolbar::Label>,
-    show_dec: Handle<CheckBox>,
-    show_hex: Handle<CheckBox>,
-    show_bin: Handle<CheckBox>,
+    increase_button: Handle<toolbar::Button>,
+    decrease_button: Handle<toolbar::Button>,
+    text: Handle<Label>,
     number: u32,
 }
 
@@ -254,60 +250,31 @@ impl MyWin {
         let mut win = MyWin {
             base: window!("'My Win',d:c,w:40,h:6"),
             increase_button: Handle::None,
-            dec: Handle::None,
-            hex: Handle::None,
-            bin: Handle::None,
-            show_dec: Handle::None,
-            show_hex: Handle::None,
-            show_bin: Handle::None,
-            number: 24,
+            decrease_button: Handle::None,
+            text: Handle::None,
+            number: 10,
         };
-        // add the increasebutton
-        win.increase_button = win.add(button!("Increase,w:15,d:l"));
-        // add checkboxes
-        win.show_dec = win.add(checkbox!("'Show decimal',x:20,y:1,w:16,checked:true"));
-        win.show_hex = win.add(checkbox!("'Show hex',x:20,y:2,w:16,checked:true"));
-        win.show_bin = win.add(checkbox!("'Show binary',x:20,y:3,w:16,checked:true"));
-        // add toolbar labels
-        let first_group = win.get_toolbar().create_group(toolbar::GroupPosition::BottomLeft);
-        let second_group = win.get_toolbar().create_group(toolbar::GroupPosition::TopRight);
-        win.dec = win.get_toolbar().add(first_group, toolbar::Label::new(""));
-        win.hex = win.get_toolbar().add(first_group, toolbar::Label::new(""));
-        win.bin = win.get_toolbar().add(second_group, toolbar::Label::new(""));
-        win.update_toolbar_labels();
+        // create a group
+        let g = win.get_toolbar().create_group(toolbar::GroupPosition::BottomRight);
+        // add buttons
+        win.increase_button = win.get_toolbar().add(g, toolbar::Button::new("+"));
+        win.decrease_button = win.get_toolbar().add(g, toolbar::Button::new("-"));
+        // add a label
+        win.text = win.add(label!("10,d:c,w:2,h:1"));
         win
     }
-    fn update_toolbale_label(&mut self, handle: Handle<toolbar::Label>, text: String) {
-        if let Some(label) = self.get_toolbar().get_mut(handle) {
-            label.set_content(text.as_str());
-        }
-    }
-    fn update_visibility_status_for_label(&mut self, handle: Handle<toolbar::Label>, visible: bool) {
-        if let Some(label) = self.get_toolbar().get_mut(handle) {
-            label.set_visible(visible);
-        }        
-    }
-    fn update_toolbar_labels(&mut self) {
-        self.update_toolbale_label(self.dec, format!("Dec:{}", self.number));
-        self.update_toolbale_label(self.hex, format!("Hex:{:X}", self.number));
-        self.update_toolbale_label(self.bin, format!("Bin:{:b}", self.number));
-    }
 }
-
-impl ButtonEvents for MyWin {
-    fn on_pressed(&mut self, _handle: Handle<Button>) -> EventProcessStatus {
-        self.number += 1;
-        self.update_toolbar_labels();
-        return EventProcessStatus::Processed;
-    }
-}
-impl CheckBoxEvents for MyWin {
-    fn on_status_changed(&mut self, handle: Handle<CheckBox>, checked: bool) -> EventProcessStatus {
+impl ToolBarEvents for MyWin {
+    fn on_button_clicked(&mut self, handle: Handle<toolbar::Button>) -> EventProcessStatus {
         match () {
-            _ if handle == self.show_bin => self.update_visibility_status_for_label(self.bin, checked),
-            _ if handle == self.show_hex => self.update_visibility_status_for_label(self.hex, checked),
-            _ if handle == self.show_dec => self.update_visibility_status_for_label(self.dec, checked),
+            _ if handle == self.increase_button => self.number += 1,
+            _ if handle == self.decrease_button => self.number -= 1,
             _ => {}
+        }
+        let h = self.text;
+        let n = self.number;
+        if let Some(label) = self.get_control_mut(h) {            
+            label.set_caption(format!("{}", n).as_str());
         }
         EventProcessStatus::Processed
     }
@@ -315,6 +282,10 @@ impl CheckBoxEvents for MyWin {
 
 fn main() -> Result<(), appcui::system::Error> {
     let mut a = App::new().build()?;
+    // let mut w = window!("Title,d:c,w:40,h:8");
+    // let g = w.get_toolbar().create_group(toolbar::GroupPosition::BottomLeft);
+    // w.get_toolbar().add(g, toolbaritem!("'&Press Me',type=button"));
+    // a.add_window(w);
     a.add_window(MyWin::new());
     a.run();
     Ok(())
