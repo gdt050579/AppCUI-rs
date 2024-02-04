cb4e855420e57b07453fdc485e675cef67b7f9b2
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-02-04 21:41:25 +0200

    remove some unnecesary files

--- appcui/src/ui/menu.rs
+++ appcui/src/ui/menu.rs
@@ -3,7 +3,6 @@ mod menu_bar;
 mod menu_bar_item;
 mod menu_button_state;
 mod menu_item;
-mod menu_item_handle;
 mod mouse_position_info;
 mod mouse_press_result;
 mod utils;
@@ -20,7 +19,6 @@ pub use self::menu::Menu;
 pub use self::menu_bar::MenuBar;
 pub(self) use self::menu_bar_item::MenuBarItem;
 pub(self) use self::menu_item::MenuItemWrapper;
-pub use self::menu_item_handle::MenuItemHandle;
 pub(crate) use self::mouse_press_result::MousePressedResult;
 
 pub use self::checkbox::CheckBox;
--- appcui/src/ui/menu/menu_item_handle.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Copy, Clone)]
-pub struct MenuItemHandle {
-    index: u32,
-}
-impl MenuItemHandle {
-    fn new(index: u32) -> Self {
-        Self { index }
-    }
-}
