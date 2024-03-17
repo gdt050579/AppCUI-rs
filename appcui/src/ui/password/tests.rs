6a9c77f9f76643b9fcd7a5020927b456e220ebad
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-03-17 11:04:19 +0200

    Password field: unit test for password events

--- appcui/src/ui/password/tests.rs
+++ appcui/src/ui/password/tests.rs
@@ -61,3 +61,74 @@ fn check_macro() {
     a.add_window(w);
     a.run();
 }
+
+#[test]
+fn check_events() {
+    #[Window(events = PasswordEvents, internal=true)]
+    struct MyWin {
+        info: Handle<Label>,
+        pass: Handle<Password>,
+    }
+    impl MyWin {
+        fn new() -> Self {
+            let mut me = Self {
+                base: Window::new("Win-1", Layout::new("d:c,w:47,h:7"), window::Flags::None),
+                info: Handle::None,
+                pass: Handle::None,
+            };
+            me.info = me.add(Label::new("<none>", Layout::new("x:0,y:0,w:35")));
+            me.pass = me.add(password!("x:1,y:2,w:30"));
+            me
+        }
+        fn set_info(&mut self, txt: &str) {
+            let h_label = self.info;
+            if let Some(label) = self.control_mut(h_label) {
+                label.set_caption(txt);
+            }
+        }
+    }
+    impl PasswordEvents for MyWin {
+        fn on_accept(&mut self, handle: Handle<Password>) -> EventProcessStatus {
+            if handle == self.pass {
+                if let Some(p) = self.control(handle) {
+                    self.set_info(format!("Acepted: {}",p.password()).as_str());
+                    return EventProcessStatus::Processed;
+                }
+            }
+            EventProcessStatus::Ignored
+        }
+    
+        fn on_cancel(&mut self, handle: Handle<Password>) -> EventProcessStatus {
+            if handle == self.pass {
+                if let Some(p) = self.control(handle) {
+                    self.set_info(format!("Canceled: {}",p.password()).as_str());                    
+                }
+                if let Some(p) = self.control_mut(handle) {
+                    p.set_password("");
+                    return EventProcessStatus::Processed;
+                }
+            }
+            EventProcessStatus::Ignored
+        }
+    }
+
+    let script = "
+        Paint.Enable(false)
+        Paint('initial state')
+        CheckHash(0xEAA261A438D9EC3D)
+        Key.TypeText(admin1234)
+        Paint('password written')
+        CheckHash(0x2DCA169E5EAD70E7)
+        CheckCursor(19,5)
+        Key.Pressed(Enter)
+        Paint('Accepted')
+        CheckHash(0xD99A474066933448)
+        Key.Pressed(Escape)
+        Paint('Cancel and empty password')
+        CheckHash(0x205EBB9FC7AB10A0)
+        CheckCursor(10,5)
+    ";
+    let mut a = App::debug(60, 10, script).build().unwrap();
+    a.add_window(MyWin::new());
+    a.run();
+}
