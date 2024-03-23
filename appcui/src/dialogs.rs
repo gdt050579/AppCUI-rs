6df3641817057850ef2f1329fa96376e82351335
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-03-23 19:28:03 +0200

    Error dialogs

--- appcui/src/dialogs.rs
+++ appcui/src/dialogs.rs
@@ -1,17 +1,21 @@
-pub mod dialog_buttons;
-pub mod dialog_result;
+mod dialog_buttons;
+mod dialog_result;
 mod generic_alert_dialog;
 
-pub use dialog_buttons::DialogButtons;
-pub use dialog_result::DialogResult;
+use dialog_buttons::DialogButtons;
+use dialog_result::DialogResult;
 use generic_alert_dialog::GenericAlertDialog;
 
 use crate::prelude::{window, ModalWindowMethods};
 
-pub fn error(title: &str, caption: &str, buttons: DialogButtons) -> DialogResult {
-    let w = GenericAlertDialog::new(title, caption, buttons, window::Type::Error);
+pub fn error(title: &str, caption: &str)  {
+    let w = GenericAlertDialog::new(title, caption, DialogButtons::Ok, window::Type::Error);
+    w.show();
+}
+pub fn retry(title: &str, caption: &str)->bool  {
+    let w = GenericAlertDialog::new(title, caption, DialogButtons::RetryCancel, window::Type::Error);
     if let Some(result) = w.show() {
-        return result;
+        return result == DialogResult::Retry;
     }
-    return DialogResult::Cancel;
+    return false;
 }
