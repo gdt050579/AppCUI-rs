00739f31aced1446a69835eb1eaa81307cf8a076
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-03-30 05:28:50 +0200

    Password control example

--- examples/password/main.rs
+++ examples/password/main.rs
@@ -22,13 +22,45 @@ impl MyWin {
 
         win
     }
+    fn check_password(&mut self) {
+        let p = self.p;
+        if let Some(pass) = self.control(p) {
+            if pass.password() == "admin" {
+                dialogs::message("Login", "Correct password. Let's start !");
+            } else {
+                if !dialogs::retry("Login", "Invalid password. Try again ?") {
+                    self.close();
+                }
+            }
+        }
+    }
 }
 
 impl ButtonEvents for MyWin {
-
+    fn on_pressed(&mut self, handle: Handle<Button>) -> EventProcessStatus {
+        match () {
+            _ if handle == self.b_cancel => {
+                self.close();
+                EventProcessStatus::Processed
+            }
+            _ if handle == self.b_ok => {
+                self.check_password();
+                EventProcessStatus::Processed
+            }
+            _ => { EventProcessStatus::Ignored }
+        }
+    }
 }
 impl PasswordEvents for MyWin {
-    
+    fn on_accept(&mut self, _: Handle<Password>) -> EventProcessStatus {
+        self.check_password();
+        EventProcessStatus::Processed
+    }
+
+    fn on_cancel(&mut self, _: Handle<Password>) -> EventProcessStatus {
+        self.close();
+        EventProcessStatus::Processed
+    }
 }
 
 fn main() -> Result<(), appcui::system::Error> {
