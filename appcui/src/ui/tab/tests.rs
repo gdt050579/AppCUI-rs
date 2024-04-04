e9e8b91df87aef80e00ea3e78b99af25f2d803f7
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-04-04 06:53:25 +0300

    Unit test - check if tab page resize workds

--- appcui/src/ui/tab/tests.rs
+++ appcui/src/ui/tab/tests.rs
@@ -1,19 +1,44 @@
 use crate::prelude::*;
 
 #[test]
-fn check_paint() {
+fn check_control_reposition() {
     let script = "
-        //Paint.Enable(false)
+        Paint.Enable(false)
         Paint('Initial state')   
-        //CheckHash(0xC656986DBDA863BA)
+        CheckHash(0x52DCC8DF3E55C403)
+        Mouse.Click(6,2,left)
+        Paint('Maximized window')
+        CheckHash(0x800E288E7DB0F28B)
+        Mouse.Move(20,1)
+        Paint('Hover over second page')
+        CheckHash(0x7D995CC4CB874301)
+        Mouse.Click(20,1,left)
+        Paint('Second page selected')
+        CheckHash(0x565503C15E91F6DB)
+        Mouse.Click(2,0,left)
+        Paint('Return to original size')
+        CheckHash(0x44CDC0ABE77E55F3)
+        Mouse.Click(40,3,left)
+        Paint('3rd page selected')
+        CheckHash(0x4D5C8439170A28E7)
+        Mouse.Click(6,2,left)
+        Paint('Maximize again')
+        CheckHash(0x8FB38F9341D9899F)
     ";
     let mut a = App::debug(60, 10, script).build().unwrap();
-    let mut w = window!("Test,d:c,w:50,h:10");
-    let mut tab = Tab::new(Layout::new("x:1,y:1,w:44,h:6"),tab::Flags::None);
+    let mut w = window!("Test,d:c,w:50,h:7,flags: Sizeable");
+    let mut tab = Tab::new(Layout::new("l:0,t:0,r:0,b:0"),tab::Flags::None);
     tab.add_tab("Page &1");
     tab.add_tab("Page &2");
     tab.add_tab("Page &3");
-    w.add(tab);
+    tab.add(0, button!("Page1-A,r:1,b:0,w:10"));
+    tab.add(0, button!("Page1-B,d:c,w:10"));    
+    tab.add(1, button!("Page2-A,r:1,b:0,w:14"));
+    tab.add(1, button!("Page2-B,d:c,w:14")); 
+    tab.add(2, button!("Page3-A,r:1,b:0,w:20"));
+    tab.add(2, button!("Page3-B,d:l,w:20"));  
+
+    w.add(tab); 
     a.add_window(w);
     a.run();
 }
