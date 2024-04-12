4e707c9f8f76cb1753dacfc2048e406990daef6b
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-04-12 10:08:44 +0300

    Unit test to check for Tab key + ReadOnly flag

--- appcui/src/ui/keyselector/tests.rs
+++ appcui/src/ui/keyselector/tests.rs
@@ -48,3 +48,42 @@ fn check_macro() {
     a.add_window(w);
     a.run();
 }
+
+#[test]
+fn check_tab_key() {
+    let script = "
+        Paint.Enable(false)
+        Mouse.Click(20,2,left)
+        Mouse.Move(0,0)
+        Paint('initial state (focus on first)')   
+        CheckHash(0x1C6AC89BA2743EFE)
+        Key.Pressed(Tab)
+        // focus should be changed because the first keyselector is readonly
+        Paint('Focus on second keyselector') 
+        CheckHash(0x3111DF60C332BBAE)
+        Key.Pressed(F1)  
+        Paint('Second key selector now has F1') 
+        CheckHash(0x38469025C1B0B847)
+        Key.Pressed(Tab)
+        Paint('Focus on 3rd keyselector') 
+        CheckHash(0x8A84DADCB73BA9D7)
+        Key.Pressed(Space)
+        Paint('3rd key selector now has Space') 
+        CheckHash(0x82CD8C72C8299691)
+        Key.Pressed(Tab)
+        // since 3rd keyselector accepts TAB - it should intercept it
+        Paint('3rd key selector now has Tab') 
+        CheckHash(0x1563BCFE7E234062)
+        Mouse.Click(20,2,left)
+        Mouse.Move(0,0)
+        Paint('Back to first')  
+        CheckHash(0x3F7B3D010FD1DF02)
+    ";
+    let mut a = App::debug(60, 10, script).build().unwrap();
+    let mut w = window!("test,d:c,w:40,h:10");
+    w.add(keyselector!("F1,x:1,y:1,w:35,h:1,flags:AcceptTab+ReadOnly"));
+    w.add(keyselector!("x:1,y:3,w:35,h:1"));
+    w.add(keyselector!("x:1,y:5,w:35,h:1,flags:AcceptTab"));
+    a.add_window(w);
+    a.run();
+}
