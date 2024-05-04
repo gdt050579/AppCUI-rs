d75a093e838b8150671aac8d362c46ef99d79670
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-05-04 13:02:41 +0300

    Empty devtest file

--- examples/dev_test.rs
+++ examples/dev_test.rs
@@ -1,23 +1,3 @@
-use appcui::prelude::*;
-
-
-#[Window()]
-struct MyWin {
-    index: u32,
-}
-impl MyWin {
-    fn new() -> Self {
-        Self {
-            base: window!("Test,x:1,y:1,w:30,h:10"),
-            index: 1,
-        }
-    }
-}
-
-
-fn main() -> Result<(), appcui::system::Error> {
-    let mut a = App::new().single_window().command_bar().menu_bar().build()?;
-    a.add_window(MyWin::new());
-    a.run();
-    Ok(())
+fn main() {
+    
 }
