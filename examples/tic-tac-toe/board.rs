40f6bb333253079bf1bc8140b1d21e8291a73d72
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-05-05 07:44:20 +0300

    Board and paint support

--- examples/tic-tac-toe/board.rs
+++ examples/tic-tac-toe/board.rs
@@ -1,12 +1,56 @@
 use appcui::prelude::*;
 
+#[derive(Copy, Clone)]
+enum Player {
+    X,
+    O,
+}
+
+#[derive(Copy, Clone)]
+enum GameState {
+    InProgress,
+    Draw,
+    Winner(Player),
+}
+
 #[CustomControl(overwrite: OnPaint+OnKeyPressed+OnMouseEvent)]
-pub struct Board {}
+pub struct Board {
+    cells: Vec<Option<Player>>,
+    status: GameState,
+}
 
 impl Board {
     pub fn new() -> Self {
         Self {
-            base: ControlBase::new(Layout::new("x:2,y:1,w:34,h:18"), true),
+            base: ControlBase::new(Layout::new("x:2,y:1,w:34,h:19"), true),
+            cells: vec![None; 9],
+            status: GameState::InProgress
+        }
+    }
+    pub fn reset_game(&mut self) {
+        for c in self.cells.iter_mut() {
+            *c = None;
+        }
+        self.status = GameState::InProgress;
+    }
+    fn paint_x(&self, x: i32, y: i32, surface: &mut Surface) {
+        let ch = char!("' ',back:red");
+        for i in 0..5 {
+            surface.write_char(x + i * 2, y + i, ch);
+            surface.write_char(x + i * 2 + 1, y + i, ch);
+            surface.write_char(x + 8 - i * 2, y + i, ch);
+            surface.write_char(x + 9 - i * 2, y + i, ch);
+        }
+    }
+    fn paint_o(&self, x: i32, y: i32, surface: &mut Surface) {
+        let ch = char!("' ',back:green");
+        surface.write_string(x + 2, y, "      ", CharAttribute::with_color(Color::Black, Color::Green), false);
+        surface.write_string(x + 2, y + 4, "      ", CharAttribute::with_color(Color::Black, Color::Green), false);
+        for i in 1..4 {
+            surface.write_char(x, y + i, ch);
+            surface.write_char(x + 1, y + i, ch);
+            surface.write_char(x + 8, y + i, ch);
+            surface.write_char(x + 9, y + i, ch);
         }
     }
 }
@@ -14,15 +58,29 @@ impl Board {
 impl OnPaint for Board {
     fn on_paint(&self, surface: &mut Surface, _theme: &Theme) {
         surface.clear(char!("' ',black,black"));
-        surface.draw_rect(Rect::new(0,0,33,17), LineType::Single, CharAttribute::with_color(Color::White, Color::Black));
+        surface.draw_rect(
+            Rect::new(0, 0, 33, 18),
+            LineType::Single,
+            CharAttribute::with_color(Color::White, Color::Black),
+        );
         surface.fill_horizontal_line(1, 6, 32, char!("BoxHorizontalSingleLine,white,black"));
         surface.fill_horizontal_line(1, 12, 32, char!("BoxHorizontalSingleLine,white,black"));
-        surface.fill_vertical_line(11, 1, 16, char!("BoxVerticalSingleLine,white,black"));
-        surface.fill_vertical_line(22, 1, 16, char!("BoxVerticalSingleLine,white,black"));
+        surface.fill_vertical_line(11, 1, 17, char!("BoxVerticalSingleLine,white,black"));
+        surface.fill_vertical_line(22, 1, 17, char!("BoxVerticalSingleLine,white,black"));
         surface.write_char(11, 6, char!("BoxCrossSingleLine,white,black"));
         surface.write_char(11, 12, char!("BoxCrossSingleLine,white,black"));
         surface.write_char(22, 6, char!("BoxCrossSingleLine,white,black"));
         surface.write_char(22, 12, char!("BoxCrossSingleLine,white,black"));
+
+        for (index, cell) in self.cells.iter().enumerate() {
+            let x = ((index % 3) as i32) * 11 + 1;
+            let y = ((index / 3) as i32) * 6 + 1;
+            match cell {
+                Some(Player::X) => self.paint_x(x, y, surface),
+                Some(Player::O) => self.paint_o(x, y, surface),
+                None => {}
+            }
+        }
     }
 }
 impl OnKeyPressed for Board {}
--- examples/tic-tac-toe/my_win.rs
+++ examples/tic-tac-toe/my_win.rs
@@ -1,5 +1,5 @@
-use appcui::prelude::*;
 use super::Board;
+use appcui::prelude::*;
 
 #[Window(events: ButtonEvents)]
 pub struct MyWin {
@@ -28,16 +28,16 @@ impl MyWin {
         let mut p1 = panel!("'First player',l:1,t:1,r:1,h:3");
         w.rb_computer = p1.add(radiobox!("&Computer,x:1,y:0,w:20,selected:true"));
         w.rb_human = p1.add(radiobox!("&Human,x:30,y:0,w:20"));
-        t.add(0,p1);
+        t.add(0, p1);
 
         // dificulty level
         let mut p2 = panel!("'Dificulty',l:1,t:5,r:1,h:3");
         w.rb_easy = p2.add(radiobox!("&Easy,x:1,y:0,w:10,selected:true"));
         w.rb_normal = p2.add(radiobox!("&Normal,x:15,y:0,w:10"));
         w.rb_hard = p2.add(radiobox!("H&ard,x:30,y:0,w:10"));
-        t.add(0,p2);
+        t.add(0, p2);
 
-        t.add(0,button!("'&Start Game',x:50%,y:100%,a:b,w:21"));
+        t.add(0, button!("'&Start Game',x:50%,y:100%,a:b,w:21"));
 
         w.board = t.add(1, Board::new());
         w.tab = w.add(t);
@@ -50,7 +50,11 @@ impl ButtonEvents for MyWin {
         let h = self.tab;
         if let Some(tab) = self.control_mut(h) {
             // switch to game time
-            tab.set_current_tab(1);            
+            tab.set_current_tab(1);
+            let b = self.board;
+            if let Some(board) = self.control_mut(b) {
+                board.reset_game();
+            }
         }
         EventProcessStatus::Processed
     }
