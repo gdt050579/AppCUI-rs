8e83f2ca1e5ccf0a07d42968d9c24dc1a6c0316c
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-05-05 06:33:53 +0300

    Prepare a Tic-Tac-Toe example

--- examples/dev_test.rs
+++ examples/dev_test.rs
@@ -1,3 +1,31 @@
-fn main() {
-    
+use appcui::prelude::*;
+
+
+#[CustomControl(overwrite = OnPaint)]
+struct MyControl<T> {
+    value: T
+}
+impl<T> MyControl<T> {
+    fn new(value: T)->Self {
+        Self {
+            base: ControlBase::new(Layout::new("d:c,w:10,h:20"), true),
+            value,
+        }
+    }
+}
+
+impl<T> OnPaint for MyControl<T> {
+    fn on_paint(&self, _surface: &mut Surface, _theme: &Theme) {}
+}
+
+
+
+
+fn main() -> Result<(), appcui::system::Error> {
+    let mut a = App::new().build()?;
+    let mut w = window!("Test,x:1,y:1,w:30,h:10");
+    w.add(MyControl::new(10));
+    a.add_window(w);
+    a.run();
+    Ok(())
 }
---
+++ examples/tic-tac-toe/main.rs
@@ -0,0 +1,13 @@
+use appcui::prelude::*;
+
+mod my_win;
+use my_win::MyWin;
+
+
+
+fn main() -> Result<(), appcui::system::Error> {
+    let mut a = App::new().size(Size::new(60, 24)).single_window().build()?;
+    a.add_window(MyWin::new());
+    a.run();
+    Ok(())
+}  
