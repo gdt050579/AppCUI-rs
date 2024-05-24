1d570ef5aa905e47df5175ea7e80a1fe2942eaab
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-05-24 08:09:01 +0300

    add setters and getters for ComboBox Item

--- appcui/src/ui/combobox/item.rs
+++ appcui/src/ui/combobox/item.rs
@@ -15,10 +15,23 @@ impl Item {
         }
         obj
     }
-    pub fn from_string(value: String, description: String)->Self {
-        Self {
-            value,
-            description,
-        }
+    pub fn from_string(value: String, description: String) -> Self {
+        Self { value, description }
+    }
+    pub fn set_value(&mut self, value: &str) {
+        self.value.clear();
+        self.value.push_str(value);
+    }
+    #[inline(always)]
+    pub fn value(&self) -> &str {
+        &self.value
+    }
+    pub fn set_description(&mut self, description: &str) {
+        self.description.clear();
+        self.description.push_str(description);
+    }
+    #[inline(always)]
+    pub fn description(&self) -> &str {
+        &self.description
     }
 }
