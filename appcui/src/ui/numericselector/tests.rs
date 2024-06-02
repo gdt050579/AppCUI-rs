da841baa48b2d8454a563cfd06832af230f8ff73
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-06-02 09:38:10 +0300

    Aditional unit tests

--- appcui/src/ui/numericselector/tests.rs
+++ appcui/src/ui/numericselector/tests.rs
@@ -76,3 +76,131 @@ fn check_create_procmacro_defaults() {
     a.add_window(w);
     a.run();
 }
+
+#[test]
+fn check_min_max_by_mouse() {
+    let script = "
+        Paint.Enable(false)
+        Paint('initial state')   
+        CheckHash(0xD4AD9150F1EA4E67)
+        Mouse.Click(4,3,left)
+        Paint('value: 4 (hover on minus)')   
+        CheckHash(0x6391031B571469C)
+        Mouse.Click(4,3,left)
+        Paint('value: 3, minus button disabled')   
+        CheckHash(0x5627CC54036AA831)
+        Mouse.Click(4,3,left)
+        Paint('value: 3, nothing changes')   
+        CheckHash(0x5627CC54036AA831)
+        Mouse.Click(20,3,left)
+        Paint('value: 4 (hover on plus)')   
+        CheckHash(0x68B6023C171AB5E4)
+        Mouse.Click(20,3,left)
+        Mouse.Click(20,3,left)
+        Mouse.Click(20,3,left)
+        Paint('value: 7, hover on plus')   
+        CheckHash(0x23F47536083C71F3)
+        Mouse.Click(20,3,left)
+        Paint('value: 8, right button disabled')   
+        CheckHash(0xC049BAA97BC1B892)
+        Mouse.Click(20,3,left)
+        Paint('value: 8, nothing changes')   
+        CheckHash(0xC049BAA97BC1B892)
+    ";
+    let mut a = App::debug(40, 10, script).build().unwrap();
+    let mut w = window!("Title,w:38,h:5,x:1,y:1,");
+    w.add(numericselector!("i32,5,min:3,max:8,step:1,x:1,y:1,w:20")); 
+    a.add_window(w);
+    a.run();
+}
+
+#[test]
+fn check_min_max_by_keyboard() {
+    let script = "
+        Paint.Enable(false)
+        Paint('initial state')   
+        CheckHash(0xD4AD9150F1EA4E67)
+        Key.Pressed(Left)
+        Paint('value: 4')   
+        CheckHash(0x154343482A32A0E)
+        Key.Pressed(Down)
+        Paint('value: 3 (minus button disabled)')   
+        CheckHash(0x5627CC54036AA831)
+        Key.Pressed(Left)
+        Paint('value: 3, nothing changes')   
+        CheckHash(0x5627CC54036AA831)
+        Key.Pressed(Right)
+        Paint('value: 4')   
+        CheckHash(0x154343482A32A0E)
+        Key.Pressed(Up)
+        Key.Pressed(Right)
+        Key.Pressed(Up)
+        Paint('value: 7')   
+        CheckHash(0xB51CDD4CCB7E5475)
+        Key.Pressed(Right)
+        Paint('value: 8, right button disabled')   
+        CheckHash(0xC049BAA97BC1B892)
+        Key.Pressed(Up)
+        Paint('value: 8, nothing changes')   
+        CheckHash(0xC049BAA97BC1B892)
+    ";
+    let mut a = App::debug(40, 10, script).build().unwrap();
+    let mut w = window!("Title,w:38,h:5,x:1,y:1,");
+    w.add(numericselector!("i32,5,min:3,max:8,step:1,x:1,y:1,w:20")); 
+    a.add_window(w);
+    a.run();
+}
+
+#[test]
+fn check_min_max_by_home_end() {
+    let script = "
+        Paint.Enable(false)
+        Paint('initial state')   
+        CheckHash(0xD4AD9150F1EA4E67)
+        Key.Pressed(Home)
+        Paint('value: 3 (minus button disabled)')   
+        CheckHash(0x5627CC54036AA831)
+        Key.Pressed(End)
+        Paint('value: 8, right button disabled')   
+        CheckHash(0xC049BAA97BC1B892)
+    ";
+    let mut a = App::debug(40, 10, script).build().unwrap();
+    let mut w = window!("Title,w:38,h:5,x:1,y:1,");
+    w.add(numericselector!("i32,5,min:3,max:8,step:1,x:1,y:1,w:20")); 
+    a.add_window(w);
+    a.run();
+}
+
+#[test]
+fn check_readonly() {
+    let script = "
+        Paint.Enable(false)
+        Paint('initial state')   
+        CheckHash(0x63F43E387E2F60B7)
+        Key.Pressed(Home)
+        Paint('nothng changes')   
+        CheckHash(0x63F43E387E2F60B7)
+        Key.Pressed(Right)
+        Key.Pressed(Up)
+        Paint('nothng changes')   
+        CheckHash(0x63F43E387E2F60B7)
+        Key.Pressed(End)
+        Paint('nothng changes')   
+        CheckHash(0x63F43E387E2F60B7)
+        Key.Pressed(Left)
+        Key.Pressed(Down)
+        Paint('nothng changes')   
+        CheckHash(0x63F43E387E2F60B7)
+        Mouse.Click(4,3,left)
+        Paint('nothng changes')   
+        CheckHash(0x63F43E387E2F60B7)
+        Mouse.Click(20,3,left)
+        Paint('nothng changes')   
+        CheckHash(0x63F43E387E2F60B7)
+    ";
+    let mut a = App::debug(40, 10, script).build().unwrap();
+    let mut w = window!("Title,w:38,h:5,x:1,y:1,");
+    w.add(numericselector!("i32,5,min:3,max:8,step:1,x:1,y:1,w:20,flags:ReadOnly")); 
+    a.add_window(w);
+    a.run();
+}
