3621081971db2bab81114430a731ccc3e0de4052
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-06-03 07:51:09 +0300

    Splitter panel

---
+++ appcui/src/ui/splitter/splitter_panel.rs
@@ -0,0 +1,14 @@
+use crate::prelude::*;
+
+#[CustomControl(internal = true)]
+pub(super) struct SplitterPanel {}
+impl SplitterPanel {
+    pub(super) fn new() -> Self {
+        Self {
+            base: ControlBase::with_status_flags(
+                Layout::new("x:0,y:0,w:100%,h:100%,a:tl"),
+                StatusFlags::Visible | StatusFlags::Enabled | StatusFlags::AcceptInput,
+            ),
+        }
+    }
+}
