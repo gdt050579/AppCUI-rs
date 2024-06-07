eebde54c4d68fb637ebd7163b70683d2bfc8683d
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-06-07 18:48:19 +0300

    Unit test for mouse actions

--- appcui/src/ui/vsplitter/tests.rs
+++ appcui/src/ui/vsplitter/tests.rs
@@ -81,3 +81,47 @@ fn check_mouse_buttons() {
     a.add_window(w);
     a.run();
 }
+
+#[test]
+fn check_mouse_movement() {
+    let script = "
+        Paint.Enable(false)        
+        //Error.Disable(true)
+        Paint('Initial state')   
+        CheckHash(0xE2548C89F72469D9)
+        Mouse.Move(30,2)
+        Paint('2. Left Button Hovered')   
+        CheckHash(0x1904C6CB28C38017)
+        Mouse.Move(30,3)
+        Paint('3. Right Button Hovered')   
+        CheckHash(0xC28A2015A90E8933)
+        Mouse.Move(30,5)
+        Paint('4. Splitter bar hovered')   
+        CheckHash(0x16A315627F3D6011)
+        Mouse.Drag(30,5,25,5)
+        Paint('5. Splitter bar moved')   
+        CheckHash(0xE4252AD91821669D)
+        Mouse.Hold(25,5,left)
+        Paint('6. Splitter bar pressed')   
+        CheckHash(0xC2F66E9DABD4BB05)
+        Mouse.Move(35,5)
+        Paint('7. Splitter bar pressed (right)')   
+        CheckHash(0xBB6A43024EE639B5)
+        Mouse.Release(35,5,left)
+        Paint('8. Splitter bar hovered')   
+        CheckHash(0x101F59917065395D)
+        Mouse.Move(30,5)
+        Paint('9. Splitter bar not-hovered')   
+        CheckHash(0x550B43D780CCCBDD)
+    ";
+    let mut a = App::debug(60, 10, script).build().unwrap();
+    let mut w = window!("Test,d:c,w:50,h:10,flags: Sizeable");
+    let mut vs = VSplitter::new(24,Layout::new("d:c,w:100%,h:100%"),vsplitter::Flags::None);
+    vs.set_min_width(vsplitter::Panel::Left, 5);
+    vs.set_min_width(vsplitter::Panel::Right, 5);
+    vs.add(vsplitter::Panel::Left,panel!("Left,l:1,r:1,t:1,b:1"));
+    vs.add(vsplitter::Panel::Right,panel!("Right,l:1,r:1,t:1,b:1"));
+    w.add(vs);
+    a.add_window(w);
+    a.run();
+}
