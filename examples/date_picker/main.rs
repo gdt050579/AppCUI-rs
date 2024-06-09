3b2a97b75ba58b816954cc5ac953c11f8f3aa0c8
Petru Vamanu <petrisor_vmy@yahoo.com>
2024-06-09 21:36:42 +0300

    Added example for DatePicker event

--- examples/date_picker/main.rs
+++ examples/date_picker/main.rs
@@ -1,10 +1,31 @@
 use appcui::prelude::*;
 
+#[Window(events=DatePickerEvents)]
+struct MyWin {
+    dp: Handle<DatePicker>,
+}
+
+impl MyWin{
+    fn new() -> Self{
+        let mut win = MyWin{
+            base: window!("Dates,d:c,w:25,h:6"),
+            dp: Handle::None,
+        };
+        win.dp = win.add(datepicker!("2024-06-13,x:1,y:1,w:19"));
+        win
+    }
+
+}
+
+impl DatePickerEvents for MyWin{
+    fn on_date_change(&mut self, _handle: Handle<DatePicker>, date: chrono::prelude::NaiveDate) -> EventProcessStatus {
+        self.base.set_title(&format!("Date: {}", date));
+        EventProcessStatus::Processed                                                                        
+    }
+}
+
 fn main(){
     let mut a =  App::new().build().unwrap();
-    let mut w = window!("Dates,d:c,w:25,h:6");
-    // w.add(DatePicker::new("2024-06-13", Layout::new("d:c,w:19")));
-    w.add(datepicker!("2024-06-13,x:1,y:1,w:19"));
-    a.add_window(w);
+    a.add_window(MyWin::new());
     a.run();
 }
