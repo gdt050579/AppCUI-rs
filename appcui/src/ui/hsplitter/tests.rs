04f5e8c5df0949f5eb83be2ad9c385403d3eef30
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-06-11 18:05:16 +0300

    Unit tests for HSplitter

--- appcui/src/ui/hsplitter/tests.rs
+++ appcui/src/ui/hsplitter/tests.rs
@@ -136,3 +136,100 @@ fn check_mouse_buttons() {
     a.add_window(w);
     a.run();
 }
+
+#[test]
+fn check_mouse_drag() {
+    let script = "
+        Paint.Enable(false)
+        Paint('Initial state (spliter at y:15)')   
+        CheckHash(0xA9EBF9E5AF1C996E)
+        Mouse.Move(10,15)
+        Paint('2. Hover over splitter')   
+        CheckHash(0xDD8DB62E8EE34896)
+        Mouse.Hold(10,15,left)
+        Paint('3. Press mouse over the splitter')   
+        CheckHash(0xEF2B96EADD67B66)
+        Mouse.Move(10,11)
+        Paint('4. Move splitter upper (spliter at y:11)')   
+        CheckHash(0x7851C97E8F4CF2A6)
+        Mouse.Move(10,0)
+        Paint('5. Top most (spliter at y:6) and highligheed')   
+        CheckHash(0x3E5331A6A833E486)
+        Mouse.Move(10,15)
+        Paint('6. Move to original position (spliter at y:15)')   
+        CheckHash(0xEF2B96EADD67B66)
+        Mouse.Move(10,55)
+        Paint('7. Bottom Most (spliter at y:24)')   
+        CheckHash(0x14CA40672CB2CAF6)
+        Mouse.Move(10,17)
+        Paint('8. Move to another pos (spliter at y:17)')   
+        CheckHash(0x1C2EDF83228C0096)
+        Mouse.Release(10,17,left)
+        Paint('9. Same position (no selection, but highlighed) (spliter at y:17)')   
+        CheckHash(0x63F99D1AAC4D3606)
+        Mouse.Move(10,6)
+        Paint('10. Same position (no selection, no highlighed) (spliter at y:17)')   
+        CheckHash(0xC1C21089B42AE13E)
+    ";
+    let mut a = App::debug(60, 30, script).build().unwrap();
+    let mut w = window!("Test,d:c,w:50,h:30,flags: Sizeable");
+    let mut hs = HSplitter::new(14,Layout::new("d:c,w:100%,h:100%"),hsplitter::ResizeBehavior::PreserveAspectRatio);
+    hs.set_min_height(hsplitter::Panel::Top, 5);
+    hs.set_min_height(hsplitter::Panel::Bottom, 4);
+    hs.add(hsplitter::Panel::Top,panel!("Top,l:1,r:1,t:1,b:1"));
+    hs.add(hsplitter::Panel::Bottom,panel!("Bottom,l:1,r:1,t:1,b:1"));
+    w.add(hs);
+    a.add_window(w);
+    a.run();
+}
+
+#[test]
+fn check_resize_preserve_aspect_ratio() {
+    let script = "
+        Paint.Enable(false)
+        Paint('Initial state (spliter at y:9)')   
+        CheckHash(0x86FBFACC75BE4F5D)
+        Mouse.Drag(54,19,54,8)
+        Paint('Same height (spliter at y:5)')   
+        CheckHash(0x4944D9828A81B459)
+    ";
+    let mut a = App::debug(60, 20, script).build().unwrap();
+    let mut w = window!("Test,d:c,w:50,h:20,flags: Sizeable");
+    w.add(hsplitter!("50%,d:c,w:100%,h:100%"));
+    a.add_window(w);
+    a.run();
+}
+
+#[test]
+fn check_resize_preserve_bottom_size() {
+    let script = "
+        Paint.Enable(false)
+        Paint('Initial state (spliter at y:13) - bottom height = 5')   
+        CheckHash(0x73FBA06DE697449D)
+        Mouse.Drag(54,19,54,8)
+        Paint('Same height (spliter at y:2) - bottom height = 5')   
+        CheckHash(0x68B1E9E699BB2E9)
+    ";
+    let mut a = App::debug(60, 20, script).build().unwrap();
+    let mut w = window!("Test,d:c,w:50,h:20,flags: Sizeable");
+    w.add(hsplitter!("75%,d:c,w:100%,h:100%,rb:PreserveBottomPanelSize"));
+    a.add_window(w);
+    a.run();
+}
+
+#[test]
+fn check_resize_preserve_top_size() {
+    let script = "
+        Paint.Enable(false)
+        Paint('Initial state (spliter at y:5) - top height = 4')   
+        CheckHash(0xB75D3BE3E241C5D)
+        Mouse.Drag(54,19,54,8)
+        Paint('Same height (spliter at y:5) - top height = 4')   
+        CheckHash(0x8138F6FBC5169A49)
+    ";
+    let mut a = App::debug(60, 20, script).build().unwrap();
+    let mut w = window!("Test,d:c,w:50,h:20,flags: Sizeable");
+    w.add(hsplitter!("25%,d:c,w:100%,h:100%,rb:PreserveTopPanelSize"));
+    a.add_window(w);
+    a.run();
+}
