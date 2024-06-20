ec6e220bd23fd1c4f905d67e72b5e3e39839299d
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-06-20 10:31:38 +0300

    Traits and events implementation

--- appcui/src/ui/listbox/events.rs
+++ appcui/src/ui/listbox/events.rs
@@ -2,10 +2,21 @@ use super::ListBox;
 use crate::{system::Handle, ui::common::traits::EventProcessStatus};
 
 pub trait ListBoxEvents {
-    fn on_something(&mut self, _handle: Handle<ListBox>) -> EventProcessStatus {
+    fn on_current_item_changed(&mut self, _handle: Handle<ListBox>, _index: usize) -> EventProcessStatus {
+        EventProcessStatus::Ignored
+    }
+    fn on_item_checked(&mut self, _handle: Handle<ListBox>, _index: usize, _checked: bool) -> EventProcessStatus {
         EventProcessStatus::Ignored
     }
 }
+#[derive(Copy,Clone)]
+pub(crate) enum ListBoxEventTypes {
+    CurrentItemChanged,
+    ItemChecked,
+}
 #[derive(Copy, Clone)]
 pub(crate) struct EventData {
+    pub(crate) event_type: ListBoxEventTypes,
+    pub(crate) index: usize,
+    pub(crate) checked: bool,
 }
