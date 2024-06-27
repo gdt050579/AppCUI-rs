f6f372cae59ee2c2c225a11c69925bff37c6b231
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-06-27 07:04:26 +0300

    Boiler plate events file for ListView

--- appcui/src/ui/listview/events.rs
+++ appcui/src/ui/listview/events.rs
@@ -1,2 +1,14 @@
 
 
+use std::any::TypeId;
+use crate::{system::Handle, ui::common::traits::EventProcessStatus};
+
+pub trait GenericListViewEvents {
+    fn on_selection_changed(&mut self, _handle: Handle<()>, _type_id: TypeId) -> EventProcessStatus {
+        EventProcessStatus::Ignored
+    }
+}
+#[derive(Copy, Clone)]
+pub(crate) struct EventData {
+    pub(crate) type_id: std::any::TypeId
+}
