ac9c41fa89d4e74f2eef1b81b70c03856cfba50d
rsimion515 <47506682+rsimion515@users.noreply.github.com>
2024-10-20 20:07:16 +0300

    removed commented code from initial commit

--- appcui/src/ui/textarea.rs
+++ appcui/src/ui/textarea.rs
@@ -4,7 +4,5 @@ pub mod events;
 #[cfg(test)]
 mod tests;
 
-//use self::selection::Selection;
 pub use self::textarea::TextArea;
 pub use self::initialization_flags::Flags;
-//pub(crate) use self::char_class::CharClass;
