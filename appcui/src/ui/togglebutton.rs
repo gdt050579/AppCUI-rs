c11abb271f9b5d2349b1ef789bfb11b5e44a5b34
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-11-17 09:13:22 +0200

    Initialization flags & type

--- appcui/src/ui/togglebutton.rs
+++ appcui/src/ui/togglebutton.rs
@@ -1,6 +1,8 @@
 mod togglebutton;
+mod initialization_flags;
 pub mod events;
 #[cfg(test)]
 mod tests;
 
 pub use self::togglebutton::ToggleButton;
+pub use self::initialization_flags::Type;
--- appcui/src/ui/togglebutton/togglebutton.rs
+++ appcui/src/ui/togglebutton/togglebutton.rs
@@ -1,35 +1,41 @@
+use flat_string::FlatString;
 use crate::prelude::*;
-use crate::ui::togglebutton::events::EventData;
+use crate::ui::togglebutton::{events::EventData, Type};
 
 #[CustomControl(overwrite=OnPaint+OnDefaultAction+OnKeyPressed+OnMouseEvent, internal=true)]
 pub struct ToggleButton {
-    caption: String,
+    caption: FlatString<22>,
     tooltip: String,
     state: bool,
+    button_type: Type,  
 }
 impl ToggleButton {
-    // pub fn new(caption: &str, tooltip: &str, layout: Layout) -> Self {
-    //     let mut but = ToggleButton {
-    //         base: ControlBase::with_status_flags(layout, StatusFlags::Visible | StatusFlags::Enabled | StatusFlags::AcceptInput),
-    //         caption: Caption::new(caption, ExtractHotKeyMethod::AltPlusKey),
-    //         state: false,
-    //     };
+    pub fn new(caption: &str, tooltip: &str, layout: Layout, button_type: Type) -> Self {
+        Self::with_selection(caption, tooltip, layout, button_type, false)
+    }
+    pub fn with_selection(caption: &str, tooltip: &str, layout: Layout, button_type: Type, selected: bool) -> Self {
+        let mut but = ToggleButton {
+            base: ControlBase::with_status_flags(layout, StatusFlags::Visible | StatusFlags::Enabled | StatusFlags::AcceptInput),
+            caption: FlatString::from_str(caption),
+            tooltip: tooltip.to_string(),
+            state: selected,
+            button_type,
+        };
 
-    //     but.set_size_bounds(1, 1, u16::MAX, 1);
-    //     but
-    // }
-    // pub fn set_caption(&mut self, caption: &str) {
-    //     self.caption.set_text(caption, ExtractHotKeyMethod::AltPlusKey);
-    //     let hotkey = self.caption.hotkey();
-    //     self.set_hotkey(hotkey);
-    // }
-    // /// Returns the toggle button caption.
-    // pub fn caption(&self) -> &str {
-    //     self.caption.text()
-    // }
+        but.set_size_bounds(1, 2, u16::MAX, 2);
+        but
+    }
+    pub fn set_caption(&mut self, caption: &str) {
+        self.caption.set(caption);
+    }
+    /// Returns the toggle button caption.
+    pub fn caption(&self) -> &str {
+        self.caption.as_str()
+    }
 }
 impl OnDefaultAction for ToggleButton {
     fn on_default_action(&mut self) {
+        self.state = !self.state;
         // self.raise_event(ControlEvent {
         //     emitter: self.handle,
         //     receiver: self.event_processor,
@@ -53,21 +59,36 @@ impl OnPaint for ToggleButton {
     fn on_paint(&self, surface: &mut Surface, theme: &Theme) {
         let col_text = match () {
             _ if !self.is_enabled() => theme.button.text.inactive,
-            _ if self.has_focus() => theme.button.text.focused,
+            _ if self.has_focus() => {
+                if self.state {
+                    CharAttribute::with_color(Color::Black, Color::Yellow)
+                } else {
+                    CharAttribute::with_color(Color::Black, Color::Silver)
+                }
+            },
             _ if self.is_mouse_over() => theme.button.text.hovered,
-            _ => theme.button.text.normal,
+            _ => {
+                if self.state {
+                    CharAttribute::with_color(Color::Yellow, Color::Transparent)
+                } else {
+                    CharAttribute::with_color(Color::Silver, Color::Transparent)
+                }
+            },
         };
-        // let w = self.size().width;
-        // let mut format = TextFormatBuilder::new()
-        //     .position((w / 2) as i32, 0)
-        //     .attribute(col_text)
-        //     .align(TextAlignament::Center)
-        //     .chars_count(self.caption.chars_count() as u16)
-        //     .wrap_type(WrapType::SingleLineWrap(w as u16))
-        //     .build();
+        let w = self.size().width;
+        let format = TextFormatBuilder::new()
+            .position((w / 2) as i32, 0)
+            .attribute(col_text)
+            .align(TextAlignament::Center)
+            .chars_count(self.caption.chars_count() as u16)
+            .wrap_type(WrapType::SingleLineWrap(w as u16))
+            .build();
 
-        // surface.clear(Character::with_attributes(' ', col_text));
-        // surface.write_text(self.caption.text(), &format);
+        surface.clear(Character::with_attributes(' ', col_text));
+        surface.write_text(self.caption.as_str(), &format);
+        if self.state {
+            surface.fill_horizontal_line(0, 1, self.size().width as i32, Character::with_attributes('▔', col_text));
+        }
     }
 }
 impl OnMouseEvent for ToggleButton {
