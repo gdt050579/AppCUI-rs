1fb5727e677598ebcbd6a83d1b83a03cef5eb51e
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-11-22 07:10:24 +0200

    Clippy fixes

--- appcui-proc-macro/src/chars/special_chars.rs
+++ appcui-proc-macro/src/chars/special_chars.rs
@@ -1737,7 +1737,7 @@ impl SpecialCharacter {
         if HASH_COLISION_VALIDATOR[entry_index] != hash {
             return None;
         }
-        return HASH_TO_ALIGNAMENT[entry_index];
+        HASH_TO_ALIGNAMENT[entry_index]
     }
     pub fn get_name(&self) -> &'static str {
         match self {
