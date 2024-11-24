fb97586732c3e6cade2c7ca6e52fca7e11f88993
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-11-24 07:53:59 +0200

    Prepare initialization code

--- appcui/src/dialogs/open_save_dialog.rs
+++ appcui/src/dialogs/open_save_dialog.rs
@@ -1,9 +1,8 @@
 use crate::prelude::*;
-
 use super::FileInfo;
 
 #[Window(events = ToggleButtonEvents+ButtonEvents, internal: true)]
-struct FileExplorer {
+pub(super) struct FileExplorer {
     list: Handle<ListView<FileInfo>>,
     details: Handle<ToggleButton>,
     columns: Handle<ToggleButton>,
@@ -13,7 +12,20 @@ struct FileExplorer {
     mask: Handle<ComboBox>,
 }
 impl FileExplorer {
-
+    pub(super) fn new(title: &str)->Self {
+        let mut w = Self {
+            base: Window::new(title, Layout::new("d:c,w:70,h:20"), window::Flags::Sizeable),
+            list: Handle::None,
+            details: Handle::None,
+            columns: Handle::None,
+            name: Handle::None,
+            b_ok: Handle::None,
+            b_cancel: Handle::None,
+            mask: Handle::None,            
+        };
+        w.set_size_bounds(40, 10, u16::MAX, u16::MAX);
+        w       
+    }
 }
 impl ButtonEvents for FileExplorer {
     fn on_pressed(&mut self, _handle: Handle<Button>) -> EventProcessStatus {
