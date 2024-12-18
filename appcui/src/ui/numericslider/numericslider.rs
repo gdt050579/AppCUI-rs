8c95cb66836178faa022c351cb8c411eb13feba9
Valentin Pinzaru <vpinzaru18@gmail.com>
2024-12-18 10:52:13 +0200

    more changes

--- appcui/src/ui/numericslider/numericslider.rs
+++ appcui/src/ui/numericslider/numericslider.rs
@@ -107,7 +107,7 @@ where
         self.y = (self.bound + 1) as f32
           / (self.m + 1) as f32;
 
-        self.nr_val = min(((self.max - self.min).abs() / self.step).cast_to_u32() + 1, self.y as u32);
+        self.nr_val = ((self.max - self.min) / self.step).cast_to_u32() + 1;
         self.sec_dim = (self.bound as f32) / ((self.nr_val - 1) as f32);
         self.p = (self.sec_dim - self.m as f32) as u32;
         self.o = (self.sec_dim - (self.m as f32 + ((self.m / 2) as f32))) as u32;
@@ -117,7 +117,7 @@ where
         }
         else {
             self.ok_step = (self.max - self.min) / T::cast_float_number(self.y as f64);
-            self.nr_val = min(((self.max - self.min).abs() / self.ok_step).cast_to_u32() + 1, self.y as u32);
+            self.nr_val = min(((self.max - self.min) / self.ok_step).cast_to_u32() + 1, self.y as u32);
             self.sec_dim = (self.bound as f32) / ((self.nr_val - 1) as f32);
             self.p = (self.sec_dim - self.m as f32) as u32;
             self.o = (self.sec_dim - (self.m as f32 + ((self.m / 2) as f32))) as u32;
@@ -133,18 +133,25 @@ where
 
         current_value = current_value + self.ok_step;
 
-        while current_value <= self.max {
+
+        for i in 1..self.nr_val - 1 {
             current_value.write_to_string(&mut string_buffer, self.format);
             self.values_string.push_str(&string_buffer);
 
-            if current_value + self.ok_step <= self.max {
+            if i != self.nr_val - 2{
                 self.values_string.push_str(&Self::get_n_spaces((self.p as usize + self.m - string_buffer.len()) as u32));
             }
 
             current_value = current_value + self.ok_step;
         }
+        current_value.write_to_string(&mut string_buffer, self.format);
+        
+        self.values_string.push_str(&Self::get_n_spaces((self.o as usize + self.m - string_buffer.len()) as u32));
+        self.values_string.push_str(&string_buffer);
+
 
         //println!("max = {}, min = {}, step = {}, size = {}, m = {}, y = {}, nr_val = {}, sec_dim = {}, p = {}, o = {}, ok_step = {}", self.max, self.min, self.step, self.bound, self.m, self.y, self.nr_val, self.sec_dim, self.p, self.o, self.ok_step);
+        //panic!("test");
     }
 
     fn get_charset_based_on_theme(&self, theme: &Theme) -> CharSet {
@@ -186,79 +193,13 @@ where
         panic!("Invalid flags received for character set!");
     }
 
-    fn get_values_count_with_custom_step(&self, step: T) -> u32 {
-        let mut error = ((self.max - self.min).cast_to_u32() % step.cast_to_u32() != 0) as u32;
-
-        return ((self.max - self.min) / step).cast_to_u32() + 1 + error;
-    }
-
-    fn find_ok_step(&mut self) {
-        let mut max_value = String::new();
-        self.max.write_to_string(&mut max_value, self.format);
-
-        let size_per_entry = max_value.len() + 1; // las un spatiu intre numere
-        let mut current_step = self.step;
-        let mut total_size = self.get_values_count_with_custom_step(current_step) * size_per_entry as u32;
-
-        if total_size <= self.bound as u32 {
-            self.ok_step = self.step;
-            return;
-        }
-
-        while total_size > self.bound as u32 && current_step < self.max {
-            current_step = current_step + T::ONE;
-            total_size = self.get_values_count_with_custom_step(current_step) * size_per_entry as u32;
-        }
-
-        self.ok_step = current_step;
-    }
-
-    fn compute_size_per_entru(&mut self) {
-        let mut string_buffer = String::with_capacity(32);
-        self.max.write_to_string(&mut string_buffer, self.format);
-        let last_nr_size: i32 = string_buffer.len() as i32;
-        if self.ok_step != self.step {
-            self.max_size_per_entry = string_buffer.len() + 1;
-            return;
-        }
-
-        let nr_of_entries: i32 = self.get_values_count_with_custom_step(self.step) as i32 - 1;
-
-        let error: i32 = {
-            if self.bound % nr_of_entries == 0 {
-                0
-            } else {
-                self.bound % nr_of_entries / nr_of_entries as i32
-            }
-        };
-        //println!("bound = {}, nr of entries = {}, error = {}, calcul magic = {}", self.bound, nr_of_entries, error, (last_nr_size / nr_of_entries + last_nr_size % nr_of_entries));
-        let error_last_element: i32 = {
-            if last_nr_size / nr_of_entries == 0 {
-                1
-            } else {
-                last_nr_size / nr_of_entries + last_nr_size % nr_of_entries
-            }
-        };
-        self.max_size_per_entry = (self.bound / nr_of_entries + error - error_last_element) as usize;
-    }
-
     pub fn set_selected_value(&mut self, value: T) {
         self.value = value;
     }
     pub fn get_selected_value(&self) -> T {
         self.value
     }
-    // fn update_cursor_pos(&mut self, x: i32) {
-    //     self.poz_triunghi = (x / self.max_size_per_entry as i32) * self.max_size_per_entry as i32;
-    //     //self.value = self.min + self.step * (x / self.max_size_per_entry as i32);
-    //     let mut c = 0;
-    //     let mut newVal = self.min;
-    //     while c < (x / self.max_size_per_entry as i32){
-    //         newVal = newVal + self.ok_step;
-    //         c += 1;
-    //     }
-    //     self.value = Self::to_interval(newVal, self.min, self.max);
-    // }
+
     fn update_cursor_pos(&mut self, x: i32){
         self.poz_triunghi = (x / self.sec_dim as i32) * self.sec_dim as i32;
         self.value = self.min + self.ok_step * Number::cast_signed_number((x / self.sec_dim as i32) as i128);
@@ -268,83 +209,6 @@ impl<T> OnPaint for NumericSlider<T>
 where
     T: Number + 'static,
 {
-    // fn on_paint(&self, surface: &mut Surface, theme: &Theme) {
-    //     //surface.clear(Character::new(SpecialChar::SingleLineDownT, Color::Blue, Color::Black, CharFlags::None));
-    //     surface.clear(char!("' ',red,black"));
-    //     let current_character_set: CharSet = self.get_charset_based_on_theme(theme);
-
-    //     let mut string_buffer = String::with_capacity(32);
-
-    //     surface.write_string(5, 0, &self.value.to_string(), theme.text.normal, false);
-    //     //surface.write_string(5, 0, &self.last_pressed_coods.y.to_string(), theme.text.normal, false);
-
-    //     //printez valorile
-    //     // x = coloana
-    //     // y = rand
-    //     let mut current_value = self.min;
-
-    //     let mut value_X = 0;
-    //     let value_Y = 2;
-    //     let mut last_column = 0;
-    //     let mut first_column = 0;
-
-    //     surface.write_char(self.poz_triunghi, 0, current_character_set.selected_value_indicator);
-
-    //     while current_value <= self.max {
-    //         current_value.write_to_string(&mut string_buffer, self.format);
-
-    //         surface.write_string(value_X, value_Y, &string_buffer, theme.text.normal, false);
-
-    //         //let indicator_pos_X = value_X + (string_buffer.len() as i32) / 2;
-    //         let indicator_pos_X = value_X;
-    //         if current_value == self.min {
-    //             surface.write_char(indicator_pos_X, value_Y - 1, current_character_set.start_char);
-    //             first_column = indicator_pos_X;
-    //         } else if current_value == self.max {
-    //             surface.write_char(indicator_pos_X, value_Y - 1, current_character_set.end_char);
-    //             last_column = indicator_pos_X;
-    //         } else {
-    //             surface.write_char(indicator_pos_X, value_Y - 1, current_character_set.value_indicator);
-    //         }
-
-    //         // if indicator_pos_X == self.last_pressed_coods.x && (value_Y - 1) == self.last_pressed_coods.y {
-    //         //     //self.set_selected_value(current_value);
-    //         //     surface.write_char(indicator_pos_X, value_Y - 2, current_character_set.selected_value_indicator);
-    //         // }
-    //         // else if current_value == self.value {
-    //         //     surface.write_char(indicator_pos_X, value_Y - 2, current_character_set.selected_value_indicator);
-    //         // }
-
-    //         value_X = value_X + string_buffer.len() as i32 + ((self.max_size_per_entry - string_buffer.len()) as i32);
-    //         if current_value == self.max {
-    //             break;
-    //         }
-    //         current_value = current_value + self.ok_step;
-    //         if current_value > self.max {
-    //             current_value = self.max;
-    //         }
-    //     }
-
-    //     for i in first_column + 1..last_column {
-    //         let mut write_separator = false;
-    //         match surface.get(i, value_Y - 1) {
-    //             Some(v) => {
-    //                 if v.code == current_character_set.start_char.code
-    //                     || v.code == current_character_set.end_char.code
-    //                     || v.code == current_character_set.value_indicator.code
-    //                 {
-    //                     write_separator = false;
-    //                 } else {
-    //                     write_separator = true;
-    //                 }
-    //             }
-    //             None => write_separator = true,
-    //         }
-    //         if write_separator {
-    //             surface.write_char(i, value_Y - 1, current_character_set.separator);
-    //         }
-    //     }
-    // }
 
     fn on_paint(&self, surface: &mut Surface, theme: &Theme) {
         let current_character_set: CharSet = self.get_charset_based_on_theme(theme);
@@ -408,9 +272,7 @@ where
             }
         };
         self.compute_math_fields();
-        //self.find_ok_step();
-        //self.compute_size_per_entru();
-        //self.poz_triunghi = (((self.value - self.min) / self.ok_step).cast_to_u32() * self.max_size_per_entry as u32) as i32;\
+
         self.poz_triunghi = (((self.value - self.min) / self.ok_step).cast_to_u32() * self.sec_dim as u32) as i32;
         self.value = Self::to_interval(self.min + self.ok_step * ((self.value - self.min) / self.ok_step), self.min, self.max);
     }
--- examples/dev_test.rs
+++ examples/dev_test.rs
@@ -6,10 +6,10 @@ use appcui::ui::common::number::*;
 
 fn main() -> Result<(), appcui::system::Error> {
     let mut a = App::new().build()?;
-    let mut w = window!("Test,d:c,w:150,h:11,flags: Sizeable");
+    let mut w = window!("Test,d:c,w:300,h:11,flags: Sizeable");
     //let mut p = panel!("Test,l:10,t:1,b:1,r:1");
     //let l = listbox!("d:c,w:100%,h:100%,flags: ScrollBars+HighlightSelectedItemWhenInactive, lsm:2, items=[1,2,3,4,5,6,7,8,9,10]");
-    let num_slider = NumericSlider::new(10, 100, 20, 30, Format::Decimal, Layout::new("x:1,y:2,w:100%"), Flags::SingleLine | Flags::HorizontalSlider);
+    let num_slider = NumericSlider::new(100, 1000, 50, 550, Format::Decimal, Layout::new("x:1,y:2,w:100%"), Flags::SingleLine | Flags::HorizontalSlider);
 
     //p.add(num_slider);
     w.add(num_slider);
