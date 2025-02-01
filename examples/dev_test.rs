be34fd6307a695bc3967b5589d2613d99ab08f57
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-02-01 19:50:12 +0200

    Empty dev tests

--- examples/dev_test.rs
+++ examples/dev_test.rs
@@ -1,33 +1,7 @@
 use appcui::prelude::*;
 
-#[derive(ListItem)]
-struct MyItem {
-    #[Column(name="Text", width=100)]
-    text: String,
-}
-impl MyItem {
-    pub fn new(text: &str) -> Self {
-        Self {
-            text: text.to_string(),
-        }
-    }
-}
-
 fn main() -> Result<(), appcui::system::Error> {
-    let mut a = App::new().build()?;
-    let mut w = window!("Tree,d:c");
-    let mut tv = treeview!("MyItem,d:c,flags: ScrollBars+SearchBar+HideHeader");
-    let h1 = tv.add(MyItem::new("Root Item 1"));    
-    let h2 = tv.add(MyItem::new("Root Item 2"));
-    let h1_1 = tv.add_to_parent(MyItem::new("First Child of Root Item 1"), h1);
-    let h1_2 = tv.add_to_parent(MyItem::new("Second Child of Root Item 1"), h1);
-    let h1_3 = tv.add_to_parent(MyItem::new("Third Child of Root Item 1"), h1);
-    let h1_1_1 = tv.add_to_parent(MyItem::new("First Child of First Child of Root Item 1"), h1_1);
-    let h2_1 = tv.add_to_parent(MyItem::new("First Child of Root Item 1"), h2);
-    let h2_2 = tv.add_to_parent(MyItem::new("Second Child of Root Item 1"), h2);
-
-    w.add(tv);
-    a.add_window(w);
+    let a = App::new().build()?;
     a.run();
     Ok(())
 }
