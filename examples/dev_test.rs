90395b61d45b840e4f8dbfeb805e3b22bb3790ce
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-02-08 15:13:10 +0200

    devtest

--- examples/dev_test.rs
+++ examples/dev_test.rs
@@ -1,7 +1,32 @@
+use std::path::{Path, PathBuf};
+
 use appcui::prelude::*;
 
+#[Window(events = ButtonEvents)]
+struct MyWin {
+
+}
+impl MyWin {
+    fn new()->Self {
+        let mut w = Self {
+            base: window!("Test,d:c")
+        };
+        w.add(button!("Press,d:c,w:14"));
+        w
+    }
+}
+impl ButtonEvents for MyWin {
+    fn on_pressed(&mut self, _: Handle<Button>) -> EventProcessStatus {
+        let p = PathBuf::from("C:\\");
+        //let p = PathBuf::from("E:\\Lucru");
+        dialogs::select_folder("Folder", dialogs::Location::Path(&p));
+        EventProcessStatus::Processed
+    }
+}
+
 fn main() -> Result<(), appcui::system::Error> {
-    let a = App::new().build()?;
+    let mut a = App::new().build()?;
+    a.add_window(MyWin::new());
     a.run();
     Ok(())
 }
