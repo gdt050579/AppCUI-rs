efab3228cb1b9056d1245bed7ed85669a8d94c43
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-02-18 07:04:41 +0200

    Aditional APIs

--- appcui/src/ui/progressbar/progressbar.rs
+++ appcui/src/ui/progressbar/progressbar.rs
@@ -5,6 +5,8 @@ pub struct ProgressBar {
     items_count: u64,
     items_processed: u64,
     proc_buf: [u8; 4],
+    text: String,
+    percentage: u8,
 }
 impl ProgressBar {
     pub fn new(items_count: u64, layout: Layout) -> Self {
@@ -13,22 +15,32 @@ impl ProgressBar {
             items_count,
             items_processed: 0,
             proc_buf: [b' ', b' ', b' ', b' '],
+            text: String::new(),
+            percentage: 0,
         }
     }
-    fn update_values(&mut self) {
+    fn update_text(&mut self, text: &str) {
+        self.text.clear();
+        self.text.push_str(text);
+    }
+    fn update_items_count(&mut self, new_count: u64) {
+        self.items_count = new_count.min(self.items_processed);
         if self.items_count == 0 {
             self.proc_buf = [b'-', b'-', b'-', b'%']
+            self.percentage = 0;
         } else {
             if self.items_processed >= self.items_count {
-                self.proc_buf = [b'1', b'0', b'0', b'%']
+                self.proc_buf = [b'1', b'0', b'0', b'%'];
+                self.percentage = 100;
             } else {
                 if self.items_processed > 0xFF_FFFF_FFFF_FFFF {
                     self.proc_buf = [b' ', b'9', b'9', b'%']
+                    self.percentage = 99;
                 } else {
-                    let proc = ((self.items_processed * 100) / self.items_count) as u8;
+                    self.percentage = ((self.items_processed * 100) / self.items_count) as u8;
                     self.proc_buf[3] = b'%';
-                    self.proc_buf[2] = (proc % 10) + 48;
-                    let proc = proc / 10;
+                    self.proc_buf[2] = (self.percentage % 10) + 48;
+                    let proc = self.percentage / 10;
                     if proc > 0 {
                         self.proc_buf[1] = (proc % 10) + 48;
                         let proc = proc / 10;
@@ -47,5 +59,6 @@ impl ProgressBar {
     }
 }
 impl OnPaint for ProgressBar {
-    fn on_paint(&self, surface: &mut Surface, theme: &Theme) {}
+    fn on_paint(&self, surface: &mut Surface, theme: &Theme) {
+    }
 }
