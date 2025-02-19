cd90ee3a3b926f1d6af30135727401403d9f099e
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-02-19 08:34:03 +0200

    Better methods for progress bar

--- appcui/src/ui/progressbar/progressbar.rs
+++ appcui/src/ui/progressbar/progressbar.rs
@@ -1,14 +1,19 @@
-use crate::prelude::*;
 use super::initialization_flags::Flags;
+use crate::prelude::*;
+use std::time::{Duration, Instant};
 
 #[CustomControl(overwrite=OnPaint, internal=true)]
 pub struct ProgressBar {
     items_count: u64,
     items_processed: u64,
     proc_buf: [u8; 4],
+    eta: [u8; 8], // hh:mm:ss
     text: String,
     percentage: u8,
     flags: Flags,
+    start: Instant,
+    extra_duration: Duration,
+    paused: bool,
 }
 impl ProgressBar {
     pub fn new(items_count: u64, layout: Layout, flags: Flags) -> Self {
@@ -17,35 +22,52 @@ impl ProgressBar {
             items_count,
             items_processed: 0,
             proc_buf: [b' ', b' ', b' ', b' '],
+            eta: [b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' '],
             text: String::new(),
             percentage: 0,
             flags,
+            start: Instant::now(),
+            extra_duration: Duration::default(),
+            paused: false,
         };
-        me.set_size_bounds(4, 1, u16::MAX, 1);
+        me.set_size_bounds(4, 1, u16::MAX, 2);
         me
     }
 
     /// Updates the text displayed on the progress bar
-    /// 
+    ///
     /// # Example
     /// ```rust
     /// use appcui::prelude::*;
-    /// 
-    /// let p = ProgressBar::new(100,Layout::new("x:1,y:1,w:20"));
+    ///
+    /// let mut p = ProgressBar::new(100,Layout::new("x:1,y:1,w:20"));
     /// p.update_text("Running ...");
     /// ```
     pub fn update_text(&mut self, text: &str) {
         self.text.clear();
         self.text.push_str(text);
     }
-    pub fn update_processed_items(&mut self, processed_items: u64) {
+
+    /// Updates the progress bar with the number of items processed so far. The progress bar will
+    /// automatically calculate the percentage and ETA and will update the display.
+    /// 
+    /// # Example
+    /// ```rust
+    /// use appcui::prelude::*;
+    /// 
+    /// let mut p = ProgressBar::new(100,Layout::new("x:1,y:1,w:20"));
+    /// p.update_progress(85);
+    /// ```
+    pub fn update_progress(&mut self, processed_items: u64) {
         self.items_processed = processed_items.min(self.items_count);
         if self.items_count == 0 {
             self.proc_buf = [b'-', b'-', b'-', b'%'];
+            self.eta = [b'-', b'-', b':', b'-', b'-', b':', b'-', b'-'];
             self.percentage = 0;
         } else {
             if self.items_processed >= self.items_count {
                 self.proc_buf = [b'1', b'0', b'0', b'%'];
+                self.eta = [b'0', b'0', b':', b'0', b'0', b':', b'0', b'0'];
                 self.percentage = 100;
             } else {
                 if self.items_processed > 0xFF_FFFF_FFFF_FFFF {
@@ -83,12 +105,38 @@ impl ProgressBar {
     pub fn items_count(&self) -> u64 {
         self.items_count
     }
-    /// Sets the totsal number of items that need to be processed
-    /// If the number of items processed is greater than the number of items to be processed, the number of items processed will be set to the number of items to be processed
+
+    /// Resumes the progress bar. This will restart the timer and will continue the progress bar
+    /// from the last position. Calling this method multiple times have no effect if the progress
+    /// bar is already running.
     #[inline(always)]
-    pub fn set_items_count(&mut self, items_count: u64) {
+    pub fn resume(&mut self) {
+        if self.paused {
+            self.start = Instant::now();
+            self.paused = false;
+        }
+    }
+
+    /// Pauses the progress bar. This will stop the timer and will pause the progress bar.
+    /// Calling this method multiple times have no effect if the progress bar is already paused.
+    #[inline(always)]
+    pub fn pause(&mut self) {
+        if !self.paused {
+            self.extra_duration += self.start.elapsed();
+            self.paused = true;
+        }
+    }
+
+    /// Resets the progress bar internal state (items processed, timer, etc). This is usefull if you
+    /// want to restart the progress bar from the beginning or if you want to reuse the same progress
+    /// bar for multiple operations.
+    #[inline(always)]
+    pub fn reset(&mut self, items_count: u64) {
         self.items_count = items_count;
-        self.update_processed_items(self.items_processed);
+        self.extra_duration = Duration::default();
+        self.start = Instant::now();
+        self.paused = false;
+        self.update_progress(0);
     }
 }
 impl OnPaint for ProgressBar {
