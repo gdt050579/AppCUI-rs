d302ea6d5190f5698a81979f819eb90b9d1f8c4e
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-03-17 20:16:43 +0200

    Empty devtest

--- examples/dev_test.rs
+++ examples/dev_test.rs
@@ -1,62 +1,7 @@
 use appcui::prelude::*;
 
-enum Status {
-    Start(u32),
-    Progress(u32),
-}
-
-#[Window(events = ButtonEvents+BackgroundTaskEvents<Status,bool>)]
-struct MyWin {
-    p: Handle<ProgressBar>,
-}
-
-impl MyWin {
-    fn new() -> Self {
-        let mut w = Self {
-            base: window!("Markdown,d:c,w:50,h:15,flags:sizeable"),
-            p: Handle::None,
-        };
-        w.p = w.add(progressbar!("l:1,t:1,r:1,h:2"));
-        w.add(button!("&Start,l:1,b:0,w:10"));
-        w
-    }
-}
-impl ButtonEvents for MyWin {
-    fn on_pressed(&mut self, _handle: Handle<Button>) -> EventProcessStatus {
-        BackgroundTask::<Status, bool>::new().run(do_something, self.handle());
-        EventProcessStatus::Processed
-    }
-}
-impl BackgroundTaskEvents<Status, bool> for MyWin {
-    fn on_update(&mut self, value: Status, _: &BackgroundTask<Status, bool>) -> EventProcessStatus {
-        let h = self.p;
-        if let Some(p) = self.control_mut(h) {
-            match value {
-                Status::Start(value) => p.reset(value as u64),
-                Status::Progress(value) => p.update_progress(value as u64),
-            }
-            EventProcessStatus::Processed
-        } else {
-            EventProcessStatus::Ignored
-        }
-    }
-}
-
-fn do_something(conector: &BackgroundTaskConector<Status, bool>) {
-    log!("TASK","Entering do_something");
-    conector.notify(Status::Start(100));
-    log!("TASK","Notify start (100)");
-    for i in 0..100 {
-        std::thread::sleep(std::time::Duration::from_millis(100));
-        log!("TASK","Notify progress ({})",i);
-        conector.notify(Status::Progress(i));
-    }
-    log!("TASK","Finish do_something");
-}
-
 fn main() -> Result<(), appcui::system::Error> {
-    let mut a = App::new().log_file("debug.log",true).build()?;
-    a.add_window(MyWin::new());
+    let a = App::new().build()?;
     a.run();
     Ok(())
 }
