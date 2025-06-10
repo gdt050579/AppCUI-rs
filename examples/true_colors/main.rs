13caeadde10b7805be82fd71df02befb9aae0899
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-06-11 00:39:48 +0300

    True color example

---
+++ examples/true_colors/main.rs
@@ -0,0 +1,33 @@
+use appcui::prelude::*;
+
+fn main() -> Result<(), appcui::system::Error> {
+    let mut app = App::new().build()?;
+    let mut w = Window::new("True Colors", Layout::new("d:c,w:70,h:19"), window::Flags::None);
+
+    let mut c = canvas!("'68x17',d:c,w:100%,h:100%");
+    c.clear_background();
+    let s = c.drawing_surface_mut();
+    s.write_string(1, 1, "Red pallete", CharAttribute::with_color(Color::White, Color::Black), false);
+    s.write_string(1, 4, "Green pallete", CharAttribute::with_color(Color::White, Color::Black), false);
+    s.write_string(1, 7, "Blue pallete", CharAttribute::with_color(Color::White, Color::Black), false);
+    s.write_string(1, 10, "Gray pallete", CharAttribute::with_color(Color::White, Color::Black), false);
+
+    for i in 0..64 {
+        let v = (i * 4) as u8;
+        s.write_char(1 + i, 2, Character::new(' ', Color::Black, Color::RGB(v, 0, 0), CharFlags::None));
+        s.write_char(1 + i, 5, Character::new(' ', Color::Black, Color::RGB(0, v, 0), CharFlags::None));
+        s.write_char(1 + i, 8, Character::new(' ', Color::Black, Color::RGB(0, 0, v), CharFlags::None));
+        s.write_char(1 + i, 11, Character::new(' ', Color::Black, Color::RGB(v, v, v), CharFlags::None));
+    }
+
+    s.write_string(1, 13, "Make sure that you compile with the 'TRUE_COLORS' feature enabled.", CharAttribute::with_color(Color::Gray, Color::Black), false);
+    s.write_string(1, 14, " If after this, you don't see various nounces or red, green, blue ", CharAttribute::with_color(Color::Gray, Color::Black), false);
+    s.write_string(1, 15, "    and white, then your terminal does not support true colors.   ", CharAttribute::with_color(Color::Gray, Color::Black), false);
+    s.write_string(38, 13, "TRUE_COLORS", CharAttribute::with_color(Color::Silver, Color::Black), false);
+    
+
+    w.add(c);
+    app.add_window(w);
+    app.run();
+    Ok(())
+}
