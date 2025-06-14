743b57867d5b7635e9949056a743d9478d5dc58f
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-06-14 08:46:53 +0300

    Refactor file system root analysis

--- appcui/src/utils/fs/windows.rs
+++ appcui/src/utils/fs/windows.rs
@@ -1,4 +1,4 @@
-use super::{RootType, Root};
+use super::{Root, RootType};
 
 #[allow(clippy::upper_case_acronyms)]
 type BOOL = u32;
@@ -6,13 +6,24 @@ type BOOL = u32;
 extern "system" {
     #[warn(non_camel_case_types)]
     fn GetLogicalDrives() -> u32;
+    #[warn(non_camel_case_types)]
     fn GetDiskFreeSpaceExA(
         lpDirectoryName: *const u8,
         lpFreeBytesAvailableToCaller: *mut u64,
         lpTotalNumberOfBytes: *mut u64,
         lpTotalNumberOfFreeBytes: *mut u64,
     ) -> BOOL;
+    #[warn(non_camel_case_types)]
+    fn GetDriveTypeA(lpRootPathName: *const u8) -> u32;
 }
+
+const DRIVE_UNKNOWN: u32 = 0;
+const DRIVE_NO_ROOT_DIR: u32 = 1;
+const DRIVE_REMOVABLE: u32 = 2;
+const DRIVE_FIXED: u32 = 3;
+const DRIVE_REMOTE: u32 = 4;
+const DRIVE_CDROM: u32 = 5;
+const DRIVE_RAMDISK: u32 = 6;
 
 pub(super) fn get_os_roots() -> Vec<Root> {
     let result = unsafe { GetLogicalDrives() };
@@ -25,16 +36,23 @@ pub(super) fn get_os_roots() -> Vec<Root> {
             let n = [64u8 + i as u8, b':', b'\\', 0];
             let mut free: u64 = 0;
             let mut total: u64 = 0;
-            let success = unsafe {
-                GetDiskFreeSpaceExA(n.as_ptr(), std::ptr::null_mut(), &mut total, &mut free) != 0
+            let success = unsafe { GetDiskFreeSpaceExA(n.as_ptr(), std::ptr::null_mut(), &mut total, &mut free) != 0 };
+            let drive_type = unsafe { GetDriveTypeA(n.as_ptr()) };
+            let root_type = match drive_type {
+                DRIVE_CDROM => RootType::CdRom,
+                DRIVE_FIXED => RootType::Fixed,
+                DRIVE_REMOVABLE => RootType::Removable,
+                DRIVE_REMOTE => RootType::Network,
+                DRIVE_RAMDISK => RootType::RamDisk,
+                _ => RootType::Unknown,
             };
             if success {
                 v.push(Root {
                     path: format!("{}:\\", (64u8 + i as u8) as char),
                     size: total,
                     free_space: free,
-                    root_type: RootType::Unknown, //GDT: use GetDriveTypeA to find the type !!!
-                    name: String::new(), // GDT: use GetVolumeInformationA to get the name/label of the drive
+                    root_type,
+                    name: String::new(),          // GDT: use GetVolumeInformationA to get the name/label of the drive
                 });
             }
         }
