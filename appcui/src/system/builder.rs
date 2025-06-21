6bf45e395cc62b4d8a7bed30813108cf2c18c5d1
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-06-21 06:56:05 +0300

    Documentation for color_schema

--- appcui/src/system/builder.rs
+++ appcui/src/system/builder.rs
@@ -37,35 +37,42 @@ impl Builder {
             use_color_schema: true,
         }
     }
+    /// Builds the application using the current settings.
     #[inline(always)]
     pub fn build(self) -> Result<App, Error> {
         App::create(self)
     }
+    /// Sets the size of the terminal.
     #[inline(always)]
     pub fn size(mut self, terminal_size: Size) -> Self {
         self.size = Some(terminal_size);
         self
     }
+    /// Sets the title of the application.
     #[inline(always)]
     pub fn title(mut self, title: &str) -> Self {
         self.title = Some(String::from(title));
         self
     }
+    /// Enables the menu bar.
     #[inline(always)]
     pub fn menu_bar(mut self) -> Self {
         self.has_menu_bar = true;
         self
     }
+    /// Enables the command bar.
     #[inline(always)]
     pub fn command_bar(mut self) -> Self {
         self.has_command_bar = true;
         self
     }
+    /// Enables the single window mode.
     #[inline(always)]
     pub fn single_window(mut self) -> Self {
         self.single_window = true;
         self
     }
+    /// Sets the desktop manager.
     #[inline(always)]
     pub fn desktop<T>(mut self, desktop: T) -> Self
     where
@@ -74,22 +81,26 @@ impl Builder {
         self.desktop_manager = Some(ControlManager::new(desktop));
         self
     }
+    /// Sets the theme of the application. If not specified, the default theme will be used.
     #[inline(always)]
     pub fn theme(mut self, theme: Theme) -> Self {
         self.theme = theme;
         self
     }
+    /// Sets the number of timers that can be used in the application.
     #[inline(always)]
     pub fn timers_count(mut self, count: u8) -> Self {
         self.max_timer_count = count.max(1); // at least one timer
         self
     }
+    /// Sets the log file where logs will be displayed. This option is used only in debug mode.
     #[inline(always)]
     pub fn log_file(mut self, name: &str, append: bool) -> Self {
         self.log_file = Some(String::from(name));
         self.log_append = append;
         self
     }
+    /// Enables or disables the use of the terminal color schema.
     #[inline(always)]
     pub fn color_schema(mut self, enabled: bool) -> Self {
         self.use_color_schema = enabled;
