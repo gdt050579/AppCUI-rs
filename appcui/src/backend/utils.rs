2c0ac25f701e3658c6fd4a98155574dee11d0ccd
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-06-30 22:03:14 +0300

    Unit tests

--- appcui/src/backend/utils.rs
+++ appcui/src/backend/utils.rs
@@ -1,6 +1,8 @@
 mod ansi_formatter;
 #[cfg(target_os = "windows")]
 pub(crate) mod win32;
+#[cfg(test)]
+mod tests;
 
 pub(crate) use ansi_formatter::AnsiFormatter;
 pub(crate) use ansi_formatter::AnsiFlags;
