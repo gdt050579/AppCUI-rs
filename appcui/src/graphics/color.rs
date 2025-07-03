45bc3049ba2575680bd09609db2046b447712db0
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-07-03 16:52:48 +0300

    Aditional unit tests

--- appcui/src/graphics/color.rs
+++ appcui/src/graphics/color.rs
@@ -132,6 +132,32 @@ impl Color {
             }
         }
     }
+    #[inline(always)]
+    pub fn inverse_color(&self) -> Self {
+        match self {
+            Color::Black       => Color::White,        // #000000 → #FFFFFF
+            Color::DarkBlue    => Color::Yellow,       // #000080 → #FFFF7F
+            Color::DarkGreen   => Color::Pink,         // #008000 → #FF7FFF
+            Color::Teal        => Color::Red,          // #008080 → #FF7F7F
+            Color::DarkRed     => Color::Aqua,         // #800000 → #7FFFFF
+            Color::Magenta     => Color::Green,        // #800080 → #7FFF7F
+            Color::Olive       => Color::Blue,         // #808000 → #7F7FFF
+            Color::Silver      => Color::Gray,         // #C0C0C0 → #3F3F3F
+            Color::Gray        => Color::Silver,       // #808080 → #7F7F7F
+            Color::Blue        => Color::Olive,        // #0000FF → #FFFF00
+            Color::Green       => Color::Magenta,      // #00FF00 → #FF00FF
+            Color::Aqua        => Color::DarkRed,      // #00FFFF → #FF0000
+            Color::Red         => Color::Teal,         // #FF0000 → #00FFFF
+            Color::Pink        => Color::DarkGreen,    // #FF00FF → #00FF00
+            Color::Yellow      => Color::DarkBlue,     // #FFFF00 → #0000FF
+            Color::White       => Color::Black,        // #FFFFFF → #000000
+            Color::Transparent => Color::Transparent,
+            #[cfg(feature = "TRUE_COLORS")]
+            Color::RGB(r, g, b) => {
+                Color::from_rgb(255 - *r, 255 - *g, 255 - *b)
+            }
+        }
+    }
     pub fn from_value(value: i32) -> Option<Color> {
         match value {
             0 => Some(Color::Black),
