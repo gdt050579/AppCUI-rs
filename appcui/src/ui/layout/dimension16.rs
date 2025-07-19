c2b6db4677c9fc952cb718db196d8130def07b6c
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-07-19 09:00:35 +0300

    From trait for dimension 16

--- appcui/src/ui/layout/dimension16.rs
+++ appcui/src/ui/layout/dimension16.rs
@@ -1,5 +1,8 @@
 use crate::utils::{KeyValuePair, ValueType};
 
+const MAX_DIMENSION: i32 = 30000;
+const MIN_DIMENSION: i32 = 0;
+
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub enum Dimension16 {
     Absolute(u16),
@@ -20,9 +23,59 @@ impl Dimension16 {
     }
     pub(super) fn new(value: &KeyValuePair) -> Option<Self> {
         match value.value_type {
-            ValueType::Number => Some(Dimension16::Absolute(value.numerical_value.clamp(0, 30000) as u16)),
-            ValueType::Percentage => Some(Dimension16::Percentage(value.numerical_value.clamp(0, 30000) as u16)),
+            ValueType::Number => Some(Dimension16::Absolute(value.numerical_value.clamp(MIN_DIMENSION, MAX_DIMENSION) as u16)),
+            ValueType::Percentage => Some(Dimension16::Percentage(value.numerical_value.clamp(MIN_DIMENSION, MAX_DIMENSION) as u16)),
             _ => None,
         }
+    }
+}
+impl From<i8> for Dimension16 {
+    fn from(value: i8) -> Self {
+        Dimension16::Absolute(value.max(0) as u16)
+    }
+}
+impl From<i16> for Dimension16 {
+    fn from(value: i16) -> Self {
+        Dimension16::Absolute(value.clamp(MIN_DIMENSION as i16, MAX_DIMENSION as i16) as u16)
+    }
+}
+impl From<i32> for Dimension16 {
+    fn from(value: i32) -> Self {
+        Dimension16::Absolute(value.clamp(MIN_DIMENSION, MAX_DIMENSION) as u16)
+    }
+}
+impl From<i64> for Dimension16 {
+    fn from(value: i64) -> Self {
+        Dimension16::Absolute(value.clamp(MIN_DIMENSION as i64, MAX_DIMENSION as i64) as u16)
+    }
+}
+impl From<u8> for Dimension16 {
+    fn from(value: u8) -> Self {
+        Dimension16::Absolute(value as u16)
+    }
+}
+impl From<u16> for Dimension16 {
+    fn from(value: u16) -> Self {
+        Dimension16::Absolute(value.min(MAX_DIMENSION as u16) as u16)
+    }
+}
+impl From<u32> for Dimension16 {
+    fn from(value: u32) -> Self {
+        Dimension16::Absolute(value.min(MAX_DIMENSION as u32) as u16)
+    }
+}
+impl From<u64> for Dimension16 {
+    fn from(value: u64) -> Self {
+        Dimension16::Absolute(value.min(MAX_DIMENSION as u64) as u16)
+    }
+}
+impl From<f64> for Dimension16 {
+    fn from(value: f64) -> Self {
+        Dimension16::Percentage(((value * 10000.0f64) as i32).clamp(MIN_DIMENSION, MAX_DIMENSION) as u16)
+    }
+}
+impl From<f32> for Dimension16 {
+    fn from(value: f32) -> Self {
+        Dimension16::Percentage(((value * 10000.0f32) as i32).clamp(MIN_DIMENSION, MAX_DIMENSION) as u16)
     }
 }
