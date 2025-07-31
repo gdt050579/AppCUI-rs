9df84d3973dcd3ffadfb8301e63b071de5dcaedf
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-08-01 00:12:24 +0300

    Aditional clippy fixes

--- examples/layout_tester/value.rs
+++ examples/layout_tester/value.rs
@@ -11,16 +11,18 @@ impl Value {
         if text.is_empty() {
             return Value::None;
         }
-        if text.ends_with('%') {
-            if let Ok(percent) = text[..text.len() - 1].parse::<f32>() {
+
+        if let Some(just_value) = text.strip_suffix('%') {
+            if let Ok(percent) = just_value.parse::<f32>() {
                 if should_be_positive && percent < 0.0f32 {
                     return Value::Error(format!("Negative percentage value are not allowed for: {field_name} ({text})"));
                 }
-                return Value::Percent(percent / 100.0f32);
+                return Value::Percent(percent / 100.0f32);                
             } else {
                 return Value::Error(format!("Invalid percentage value: {field_name} ({text})"));
             }
         }
+        
         if let Ok(integer) = text.parse::<i32>() {
             if should_be_positive && integer < 0 {
                 return Value::Error(format!("Negative value are not allowed for: {field_name} ({text})"));
