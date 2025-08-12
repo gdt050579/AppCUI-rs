4dce3587ebbbf6c1715a10630c0362ee474e9e88
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-08-12 22:53:06 +0300

    char renderer support

--- appcui/src/ui/charpicker/charpicker.rs
+++ appcui/src/ui/charpicker/charpicker.rs
@@ -6,6 +6,8 @@ pub struct CharPicker {
     code: char,
     header_y_ofs: i32,
     expanded_panel_y: i32,
+    chars_per_width: i32,
+    start_code: u32,
 }
 impl CharPicker {
     pub fn new(code: char, layout: Layout) -> Self {
@@ -13,7 +15,9 @@ impl CharPicker {
             base: ControlBase::with_status_flags(layout, StatusFlags::Visible | StatusFlags::Enabled | StatusFlags::AcceptInput),
             header_y_ofs: 0,
             expanded_panel_y: 1,
+            chars_per_width: 1,
             code,
+            start_code: 32,
         };
         cp.set_size_bounds(11, 1, u16::MAX, 1);
         cp
@@ -34,12 +38,12 @@ impl OnPaint for CharPicker {
         // normal bar
         surface.fill_horizontal_line_with_size(0, self.header_y_ofs, size.width.saturating_sub(4), space_char);
         surface.write_char(1, self.header_y_ofs, Character::with_attributes(self.code, col_text));
-        let mut arr: [u8;9] = [b'(', b'U', b'+', b'0', b'0', b'0', b'0', b'0',b')'];
+        let mut arr: [u8; 9] = [b'(', b'U', b'+', b'0', b'0', b'0', b'0', b'0', b')'];
         let mut code = self.code as u32;
         let mut pos = 7;
-        while (code > 0) && (pos>2) {
+        while (code > 0) && (pos > 2) {
             let r = (code % 16) as u8;
-            if r<10 {
+            if r < 10 {
                 arr[pos] = 48 + r;
             } else {
                 arr[pos] = 55 + r;
@@ -48,8 +52,8 @@ impl OnPaint for CharPicker {
             code = code >> 4;
         }
         // paint code
-        if size.width>12 {
-            if size.width<17 {
+        if size.width > 12 {
+            if size.width < 17 {
                 surface.write_ascii(3, self.header_y_ofs, &arr[3..8], col_text, false);
             } else {
                 surface.write_ascii(3, self.header_y_ofs, arr.as_slice(), col_text, false);
@@ -59,6 +63,45 @@ impl OnPaint for CharPicker {
         let px = (size.width - 3) as i32;
         surface.fill_horizontal_line_with_size(px, self.header_y_ofs, 3, space_char);
         surface.write_char(px + 1, self.header_y_ofs, Character::with_attributes(SpecialChar::TriangleDown, col_text));
+        // assuming the control is expanded
+        if self.is_expanded() {
+            let size = self.expanded_size();
+            let col = theme.menu.text.normal;
+            surface.fill_rect(
+                Rect::with_size(0, self.expanded_panel_y, size.width as u16, (size.height - 1) as u16),
+                space_char,
+            );
+            surface.draw_rect(
+                Rect::with_size(0, self.expanded_panel_y, size.width as u16, (size.height - 1) as u16),
+                LineType::Single,
+                col,
+            );
+            surface.draw_horizontal_line(1, self.expanded_panel_y + 2, size.width as i32 - 2, LineType::Single, col);
+            let mut y = 4;
+            let mut x = 0;
+            let mut code = self.start_code;
+            let end_code = code + 1000;
+            loop {
+                let ch = if (code == 128) || (code == 0) {
+                    ' '
+                } else {
+                    char::from_u32(code).unwrap_or('?')
+                };
+                surface.write_char(x * 3 + 1, y, Character::with_attributes(ch, col));
+                x += 1;
+                if x >= self.chars_per_width {
+                    x = 0;
+                    y += 1;
+                    if ((y as u32) + 1) >= size.height {
+                        break;
+                    }
+                }
+                code += 1;
+                if code >= end_code {
+                    break;
+                }
+            }
+        }
     }
 }
 impl OnDefaultAction for CharPicker {
@@ -82,6 +125,7 @@ impl OnExpand for CharPicker {
                 self.header_y_ofs = 0;
             }
         }
+        self.chars_per_width = (self.expanded_size().width / 3) as i32;
     }
     fn on_pack(&mut self) {
         self.expanded_panel_y = 1;
