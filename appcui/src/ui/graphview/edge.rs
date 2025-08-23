cd8df8222f1a61d56ce3a7d7fcea788e359a627b
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-08-23 10:12:34 +0300

    Aditional methods

--- appcui/src/ui/graphview/edge.rs
+++ appcui/src/ui/graphview/edge.rs
@@ -2,3 +2,8 @@ pub struct Edge {
     pub(super) from_node_id: u32,
     pub(super) to_node_id: u32,
 }
+impl Edge {
+    pub fn bidirectional(from_node_id: u32, to_node_id: u32) -> Self {
+        Self { from_node_id, to_node_id }
+    }
+}
