618867e1de20486695bf7889a7df0383b34af693
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-09-03 07:53:07 +0300

    graphview! procmacro support

--- appcui/src/ui/graphview/tests.rs
+++ appcui/src/ui/graphview/tests.rs
@@ -23,3 +23,20 @@ fn sinple_display() {
     a.add_window(w);
     a.run();
 }
+
+#[test]
+fn sinple_display_proc_macro() {
+    let script = "
+        Paint.Enable(false)
+        Paint('Initial state')   
+        CheckHash(0xC976C47E78769FFC) 
+    ";
+    let mut a = App::debug(60, 10, script).build().unwrap();
+    let mut w = window!("Test,d:f");
+    let mut gv = graphview!("line-type: SingleThick, routing: Orthogonal, hie: true, hoe: true, arrows: false, arrange: Hierarchical, back:{.,gray,black}, d:f, flags:[ScrollBars,SearchBar],lsm:2,tsm:1");
+    let g = graphview::Graph::with_slices(&["A","B","C","D"], &[(0,1),(0,2),(1,3)], true);
+    gv.set_graph(g);
+    w.add(gv);
+    a.add_window(w);
+    a.run();
+}
