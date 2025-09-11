f7a649b7f9cc0990451a6391fe6a4714a9a9bcee
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-09-11 10:30:52 +0300

    Selectors

--- examples/basic_controls/main.rs
+++ examples/basic_controls/main.rs
@@ -1,5 +1,47 @@
 use appcui::prelude::*;
 
+struct MyObject {
+    name: String,
+    description: String,
+    symbol: String,
+}
+
+impl MyObject {
+    fn new(name: &str, description: &str, symbol: &str) -> MyObject {
+        MyObject {
+            name: name.to_string(),
+            description: description.to_string(),
+            symbol: symbol.to_string(),
+        }
+    }
+}
+
+impl DropDownListType for MyObject {
+    fn name(&self) -> &str {
+        &self.name
+    }
+    fn description(&self) -> &str {
+        &self.description
+    }
+    fn symbol(&self) -> &str {
+        &self.symbol
+    }
+}
+
+#[derive(EnumSelector, Eq, PartialEq, Copy, Clone)]
+enum Shape {
+    #[VariantInfo(name = "Square", description = "a red square")]
+    Square,
+
+    #[VariantInfo(name = "Rectangle", description = "a green rectangle")]
+    Rectangle,
+
+    #[VariantInfo(name = "Triangle", description = "a blue triangle")]
+    Triangle,
+
+    #[VariantInfo(name = "Circle", description = "a white circle")]
+    Circle,
+}
 
 fn add_description(tab: &mut Tab, index: u32, desc: &str) {
     tab.add(index, vline!("r:30,t:0,b:0"));
@@ -60,6 +102,35 @@ fn add_radioboxes(tab: &mut Tab, index: u32) {
 fn add_selectors(tab: &mut Tab, index: u32) {
     add_description(tab, index, "Selctors are component that allows you to chose from multiple variants (such as colors, characters, list of options or numbers).");
     tab.add(index, label!("'Combobox (drop-down)',x:1,y:1, w: 20"));
+    let mut c = ComboBox::new(layout!("x:25,y:1,w:30"), combobox::Flags::ShowDescription);
+    // data from https://en.wikipedia.org/wiki/Fastest_animals
+    c.add_item(combobox::Item::new("Cheetah","(120 km/h)"));
+    c.add_item(combobox::Item::new("Swordfish","(97 km/h)"));
+    c.add_item(combobox::Item::new("Iguana","(35 km/h)"));
+    c.add_item(combobox::Item::new("Gazelle","(81 km/h)"));
+    c.add_item(combobox::Item::new("Lion","(80 km/h)"));
+    c.add_item(combobox::Item::new("Dog","(60 km/h)"));
+    c.add_item(combobox::Item::new("Zebra","(56 km/h)"));  
+    tab.add(index, c);  
+
+    tab.add(index, label!("'Drop down list',x:1,y:3, w: 20"));
+    let mut db = DropDownList::<MyObject>::with_symbol(1, layout!("x:25,y:3,w:30"), dropdownlist::Flags::ShowDescription);
+    db.add(MyObject::new("Heart", "(symbol of love)", "♥"));
+    db.add(MyObject::new("Spade", "(used in a deck of cards)", "♠"));
+    tab.add(index, db); 
+
+    tab.add(index, label!("'Enum selector',x:1,y:5, w: 20"));
+    tab.add(index, selector!("enum: Shape,x:25,y:5,w:30, flags: AllowNoneVariant"));    
+
+    tab.add(index, label!("'Color picker',x:1,y:7, w: 20"));
+    tab.add(index, colorpicker!("Yellow,x:25,y:7,w:30")); 
+
+    tab.add(index, label!("'Date selector',x:1,y:9, w: 20"));
+    tab.add(index, datepicker!("date:2025-09-09,x:25,y:9,w:30"));    
+
+    tab.add(index, label!("'Character picker',x:1,y:11, w: 20"));
+    tab.add(index, charpicker!("code=65,x:25,y:11,w:30,sets=[Ascii,Arrows,Emoticons,Shapes]"));    
+
 }
 
 
@@ -71,6 +142,7 @@ fn main() -> Result<(), appcui::system::Error> {
     add_buttons(&mut t, 0);
     add_checkboxes(&mut t, 1);
     add_radioboxes(&mut t, 2);
+    add_selectors(&mut t, 3);
     w.add(t);
     a.add_window(w);
     a.run();
