5a520ef10d94ff051ad96e87e0f6c5b52fb85416
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-09-27 06:15:20 +0300

    Empty dev_test example

--- examples/dev_test.rs
+++ examples/dev_test.rs
@@ -1,12 +1,7 @@
 use appcui::prelude::*;
 
-
 fn main() -> Result<(), appcui::system::Error> {
-    let mut app = App::new().app_bar().build()?;
-    let mut w = window!("'Test',a:c,w:30,h:10");
-    let tp = TimePicker::new(12, 30, 0, layout!("x:1,y:1,w:10,h:1"), timepicker::Flags::Seconds);
-    w.add(tp);  
-    app.add_window(w);
+    let app = App::new().app_bar().build()?;
     app.run();
     Ok(())
 }
