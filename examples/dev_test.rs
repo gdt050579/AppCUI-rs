e73d9146f96f565bb1883ead7f1a28138f38ea7c
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-10-08 07:37:51 +0300

    Bullets type for radioboxes

--- examples/dev_test.rs
+++ examples/dev_test.rs
@@ -1,7 +1,11 @@
 use appcui::prelude::*;
 
 fn main() -> Result<(), appcui::system::Error> {
-    let app = App::new().app_bar().build()?;
+    let mut app = App::new().app_bar().build()?;
+    let mut w = Window::with_type("Test", layout!("a:c,w:40,h:10"), window::Flags::Sizeable, Some(window::Type::Round), window::Background::Normal);
+    w.set_tag("win");
+    w.set_hotkey(key!("Alt+1"));
+    app.add_window(w);
     app.run();
     Ok(())
 }
--- examples/radioboxes_types/main.rs
+++ examples/radioboxes_types/main.rs
@@ -1,9 +1,13 @@
 use appcui::prelude::*;
 
 fn main() -> Result<(), appcui::system::Error> {
-    let mut app = App::new().build()?;
+    #[cfg(target_os = "windows")]
+    let mut app = App::with_backend(appcui::backend::Type::WindowsVT).color_schema(false).build()?;
+    #[cfg(not(target_os = "windows"))]
+    let mut app = App::new().color_schema(false).build()?;
+
     let mut win = window!("'Radiobox types',a:c,w:80,h:15, flags:Sizeable");
-    let mut acc = accordion!("d:f,panels:['&Standard', '&Circle', '&Diamond', '&Ascii']");
+    let mut acc = accordion!("d:f,panels:['&Standard', '&Circle', '&Diamond', '&Ascii', '&Bullet']");
     
     // Standard
     acc.add(0, radiobox!("'Option 1 (not-selected)',x:1,y:1,w:40,selected:false"));
@@ -24,6 +28,11 @@ fn main() -> Result<(), appcui::system::Error> {
     acc.add(3, radiobox!("'Option 1 (not-selected)',x:1,y:1,w:40,type=Ascii,selected:false"));
     acc.add(3, radiobox!("'Option 2 (selected)',x:1,y:2,w:40,type=Ascii,selected:true"));
     acc.add(3, radiobox!("'Option 3 (disabled and not-selected)',x:1,y:3,w:40,type=Ascii,selected:false, enabled:false"));
+
+    // Bullet
+    acc.add(4, radiobox!("'Option 1 (not-selected)',x:1,y:1,w:40,type=Bullet,selected:false"));
+    acc.add(4, radiobox!("'Option 2 (selected)',x:1,y:2,w:40,type=Bullet,selected:true"));
+    acc.add(4, radiobox!("'Option 3 (disabled and not-selected)',x:1,y:3,w:40,type=Bullet,selected:false, enabled:false"));
 
     win.add(acc);
     app.add_window(win);
