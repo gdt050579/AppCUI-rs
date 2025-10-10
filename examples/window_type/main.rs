78608a484f5c7cc3918a68ec1b81c548f2168468
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-10-10 23:01:48 +0300

    Fix example

--- examples/window_type/main.rs
+++ examples/window_type/main.rs
@@ -3,17 +3,17 @@ use appcui::prelude::*;
 fn main() -> Result<(), appcui::system::Error> {
     let mut app = App::new().build()?;
 
-    let mut w = Window::with_type("Type::Panel", layout!("x:1,y:2,w:40,h:10"), window::Flags::Sizeable, Some(window::Type::Panel), window::Background::Normal);
+    let mut w = Window::with_type("Type::Panel", layout!("x:1,y:2,w:40,h:10"), window::Flags::Sizeable, window::Type::Panel, window::Background::Normal);
     w.set_tag("win");
     w.set_hotkey(key!("Alt+1"));
     app.add_window(w);
 
-    let mut w = Window::with_type("Type::Round", layout!("x:50,y:2,w:40,h:10"), window::Flags::Sizeable, Some(window::Type::Rounded), window::Background::Normal);
+    let mut w = Window::with_type("Type::Rounded", layout!("x:50,y:2,w:40,h:10"), window::Flags::Sizeable, window::Type::Rounded, window::Background::Normal);
     w.set_tag("win");
     w.set_hotkey(key!("Alt+2"));
     app.add_window(w);
 
-    let mut w = Window::with_type("Type::Normal", layout!("x:1,y:15,w:40,h:10"), window::Flags::Sizeable, Some(window::Type::Classic), window::Background::Normal);
+    let mut w = Window::with_type("Type::Classic", layout!("x:1,y:15,w:40,h:10"), window::Flags::Sizeable, window::Type::Classic, window::Background::Normal);
     w.set_tag("win");
     w.set_hotkey(key!("Alt+3"));
     app.add_window(w);
