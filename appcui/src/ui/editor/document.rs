be3fc6fefecbe66c471784804e27914bd0c12b1c
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2026-05-16 16:24:15 +0300

    some fixes (new line at the end).

--- appcui/src/ui/editor/document.rs
+++ appcui/src/ui/editor/document.rs
@@ -1,6 +1,6 @@
-use ropey::{Rope, RopeSlice};
-use ropey::iter::{Lines, Chars};
 use super::LineCharIndex;
+use ropey::iter::{Chars, Lines};
+use ropey::{Rope, RopeSlice};
 
 pub(super) struct PosInfo {
     pub(super) line_index: u32,
@@ -20,9 +20,6 @@ impl Document {
     pub(super) fn line(&self, line_index: u32) -> RopeSlice<'_> {
         self.rope.line(line_index as usize)
     }
-    // pub(super) fn lines_starting_from(&self, line_index: u32) -> Lines<'_> {
-    //     self.rope.lines_at(line_index as usize)
-    // }
     pub(super) fn line_to_char(&self, line_index: u32) -> usize {
         self.rope.line_to_char(line_index as usize)
     }
@@ -39,10 +36,21 @@ impl Document {
         self.rope.char_to_line(char_index) as u32
     }
     pub(super) fn line_end_position(&self, line: u32) -> usize {
-        let ln = self.line(line);
         let start = self.line_to_char(line);
-        start + ln.len_chars().saturating_sub(1)
-    } 
+        let ln = self.line(line);
+        let len = ln.len_chars();
+        if len == 0 {
+            return start;
+        }
+        // last_linee checks
+        if (line as usize) + 1 == self.rope.len_lines() {
+            let ch = ln.char(len - 1);
+            if ch == '\n' || ch == '\r' {
+                return start + len;
+            }
+        }
+        start + len - 1
+    }
     pub(super) fn char_to_pos_info(&self, char_index: usize) -> PosInfo {
         let line_index = self.position_to_line(char_index);
         let start = self.rope.line_to_char(line_index as usize);
