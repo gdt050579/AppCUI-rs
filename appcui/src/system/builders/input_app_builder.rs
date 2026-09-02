2c5391721401aafa479dccc086ef648a544956c6
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2026-09-02 12:15:06 +0300

    input app skelet

--- appcui/src/system/builders/input_app_builder.rs
+++ appcui/src/system/builders/input_app_builder.rs
@@ -5,21 +5,18 @@ use super::impl_app_desktop_methods;
 use super::impl_terminal_builder_methods;
 use super::InternalBuilder;
 
-pub trait FrameApp {
+pub trait InputApp {
     /// Called once, after the app is created, before the first paint.
     fn on_start(&mut self) {}
 
     /// Called when the terminal/surface size changes.
     fn on_resize(&mut self, _new_size: Size) {}
 
-    /// Called each tick at the configured rate (default 60/sec), with the number of ticks since the beginning of the application.
-    fn on_update(&mut self, _ticks: u64) {}
-
     /// Called when a key is pressed.
-    fn on_key_event(&mut self, _key: Key, _ch: char) {}
+    fn on_key_event(&mut self, _key: Key, _ch: char) -> EventProcessStatus { EventProcessStatus::Ignore }
 
     /// Called when a mouse event occurs.
-    fn on_mouse_event(&mut self, _ev: &MouseEvent) {}
+    fn on_mouse_event(&mut self, _ev: &MouseEvent) -> EventProcessStatus { EventProcessStatus::Ignore }
 
     /// Draw current state to the surface.
     fn on_paint(&self, surface: &mut Surface);
@@ -33,37 +30,35 @@ pub trait FrameApp {
 #[repr(C)]
 struct AppDesktop<T>
 where
-    T: FrameApp,
+    T: InputApp,
 {
     base: Desktop,
-    frame_app: T,
-    fps: u32,
+    input_app: T,
     auto_close: bool,
     clear_char: Option<Character>,
 }
-impl_app_desktop_methods!(AppDesktop, FrameApp);
-impl<T: FrameApp> AppDesktop<T> {
-    fn new(frame_app: T, fps: u32, auto_close: bool, clear_char: Option<Character>) -> Self {
+impl_app_desktop_methods!(AppDesktop, InputApp);
+impl<T: InputApp> AppDesktop<T> {
+    fn new(input_app: T,  auto_close: bool, clear_char: Option<Character>) -> Self {
         Self {
             base: Desktop::new(),
-            frame_app,
-            fps,
+            input_app,
             auto_close,
             clear_char,
         }
     }
 }
-impl<T: FrameApp> OnPaint for AppDesktop<T> {
+impl<T: InputApp> OnPaint for AppDesktop<T> {
     fn on_paint(&self, surface: &mut Surface, _: &Theme) {
         if let Some(ch) = self.clear_char {
             surface.reset(ch);
         } else {
             surface.reset_clip_and_origin();
         }
-        self.frame_app.on_paint(surface);
+        self.input_app.on_paint(surface);
     }
 }
-impl<T: FrameApp> OnKeyPressed for AppDesktop<T> {
+impl<T: InputApp> OnKeyPressed for AppDesktop<T> {
     fn on_key_pressed(&mut self, key: Key, character: char) -> EventProcessStatus {
         if self.auto_close {
             if key.value() == key!("Escape") {
@@ -71,30 +66,28 @@ impl<T: FrameApp> OnKeyPressed for AppDesktop<T> {
                 return EventProcessStatus::Processed;
             }
         }
-        self.frame_app.on_key_event(key, character);
-        EventProcessStatus::Ignored
+        self.input_app.on_key_event(key, character);
     }
 }
-impl<T: FrameApp> OnMouseEvent for AppDesktop<T> {
+impl<T: InputApp> OnMouseEvent for AppDesktop<T> {
     fn on_mouse_event(&mut self, event: &MouseEvent) -> EventProcessStatus {
-        self.frame_app.on_mouse_event(event);
-        EventProcessStatus::Ignored
+        self.input_app.on_mouse_event(event)
     }
 }
-impl<T: FrameApp> OnResize for AppDesktop<T> {
+impl<T: InputApp> OnResize for AppDesktop<T> {
     fn on_resize(&mut self, _: Size, new_size: Size) {
-        self.frame_app.on_resize(new_size);
+        self.input_app.on_resize(new_size);
     }
 }
-impl<T: FrameApp> DesktopEvents for AppDesktop<T> {
+impl<T: InputApp> DesktopEvents for AppDesktop<T> {
     fn on_start(&mut self) {
         let milis = (1000u32 / self.fps) as u64;
         self.timer().unwrap().start(Duration::from_millis(milis));
-        self.frame_app.on_start();
+        self.input_app.on_start();
     }
 
     fn on_close(&mut self) -> ActionRequest {
-        if self.frame_app.on_close() == ActionRequest::Allow {
+        if self.input_app.on_close() == ActionRequest::Allow {
             self.timer().unwrap().stop();
             ActionRequest::Allow
         } else {
@@ -103,39 +96,26 @@ impl<T: FrameApp> DesktopEvents for AppDesktop<T> {
     }
 }
 
-impl<T: FrameApp> TimerEvents for AppDesktop<T> {
-    fn on_update(&mut self, ticks: u64) -> EventProcessStatus {
-        self.frame_app.on_update(ticks);
-        EventProcessStatus::Processed
+impl<T: InputApp> TimerEvents for AppDesktop<T> {
+    fn on_update(&mut self, _: u64) -> EventProcessStatus {
+        EventProcessStatus::Ignore
     }
 }
-pub struct FrameAppBuilder<T: FrameApp + 'static> {
+pub struct InputAppBuilder<T: InputApp + 'static> {
     builder: InternalBuilder,
-    fps: u32,
     auto_close: bool,
     clear_screen_char: Option<Character>,
-    frame_app: T,
+    input_app: T,
 }
 
-impl<T: FrameApp + 'static> FrameAppBuilder<T> {
-    pub(crate) fn new(frame_app: T) -> Self {
+impl<T: InputApp + 'static> InputAppBuilder<T> {
+    pub(crate) fn new(input_app: T) -> Self {
         Self {
             builder: InternalBuilder::new(),
-            fps: 30,
             auto_close: true,
             clear_screen_char: Some(char!("' ',white,black")),
-            frame_app,
+            input_app,
         }
-    }
-    /// Sets the frame rate of the application.
-    /// The frame rate is the number of frames per second that the application will run at.
-    /// The default frame rate is 30 FPS.
-    /// The maximum frame rate is 120 FPS.
-    /// The minimum frame rate is 1 FPS.
-    #[inline(always)]
-    pub fn fps(mut self, fps: u32) -> Self {
-        self.fps = fps.clamp(1, 120);
-        self
     }
 
     #[inline(always)]
@@ -154,7 +134,7 @@ impl<T: FrameApp + 'static> FrameAppBuilder<T> {
     #[inline(always)]
     pub fn run(mut self) -> Result<(), crate::system::Error> {
         self.builder
-            .desktop(AppDesktop::new(self.frame_app, self.fps, self.auto_close, self.clear_screen_char));
+            .desktop(AppDesktop::new(self.input_app, self.fps, self.auto_close, self.clear_screen_char));
         let app = self.builder.build()?;
         app.start_app()
     }
