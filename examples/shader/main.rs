9209551fcc18b6afba371bdccb9f46bcfec4891d
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2026-09-04 08:41:35 +0300

    Unix example fix

--- examples/shader/main.rs
+++ examples/shader/main.rs
@@ -189,7 +189,7 @@ fn main() -> Result<(), appcui::system::Error> {
         App::frame_app(Shader::new())
             .fps(FPS)
             .title("Shader")
-            .background_char(None)
+            .clear_char(None)
             .color_schema(false)
             .run()
     }
